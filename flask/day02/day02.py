0073d5f054fcd6036e682f7e4391f773a7703594
anthropologistbiubiu <1319847957@qq.com>
2023-07-22 23:43:17 +0800

    modify

--- flask/day02/day02.py
+++ flask/day02/day02.py
@@ -25,12 +25,54 @@ def get_data():
         "age": age
     }
     return jsonify(result)
-
+# curl localhost:8080/api/data -X POST -d '{"hello": "world"}' --header "Content-Type: application/json"
 @app.route('/api/data', methods=['POST'])
 def update_data():
     new_data = request.get_json()  # 获取POST请求中的JSON数据
     print(new_data)
     return jsonify(new_data)
 
+
+# curl "http://127.0.0.1:8080/api/data?name=John&age=30"
+
+
+@app.route('/submit/x-www-form-urlencode', methods=['POST'])
+def submit_xwwwformurlencod():
+    # Get form parameters
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+    print(name,email,message)
+    # Perform data processing or business logic here (if needed)
+    # ...
+    # Prepare response data
+    response_data = {
+        "name": name,
+        "email": email,
+        "message": message
+    }
+    return jsonify(response_data)
+# curl -X POST -d "name=John&email=john@example.com&message=Hello" http://127.0.0.1:8080/submit
+
+
+@app.route('/submit/formdata', methods=['POST'])
+def submit_formdata():
+    # Get form parameters
+    name = request.form.get('name')
+    email = request.form.get('email')
+    message = request.form.get('message')
+
+    # Perform data processing or business logic here (if needed)
+    # ...
+
+    # Prepare response data
+    response_data = {
+        "name": name,
+        "email": email,
+        "message": message
+    }
+
+    return jsonify(response_data)
+# curl -X POST -F "name=sunweiming" -F "email=1319847957qq.com" -F "message=Hello" http://127.0.0.1:8080/submit/formdata
 if __name__ == '__main__':
     app.run(debug=True,port=8080)
