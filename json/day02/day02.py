ab02e671da5dbe6ddcc8699da99ab0562955f8a0
anthropologistbiubiu <1319847957@qq.com>
2023-07-22 20:25:47 +0800

    modify

--- json/day02/day02.py
+++ json/day02/day02.py
@@ -21,7 +21,41 @@ def obj_To_Json():
     print(json_dict['is_student'])
     pass
 def json_To_Dict():
+    person_dict = {'name': 'John Doe', 'age': 30, 'is_student': True}
 
+    # 将Python字典转换为JSON字符串
+    json_str = json.dumps(person_dict)
+    print(json_str)
     pass
+# 处理JSON数组
+def json_To_List():
+    json_arr = '["apple", "banana", "orange"]'
+
+    # 将JSON数组解析为Python列表
+    py_list = json.loads(json_arr)
+
+    # 输出Python列表
+    print(py_list)
+    print(type(py_list))
+def list_To_Json():
+    # Python列表
+    py_list = ["apple", "banana", "orange"]
+    # 将Python列表转换为JSON数组
+    json_arr = json.dumps(py_list)
+    # 输出JSON数组
+    print(json_arr)
+    print(type(json_arr))
+def tuple_to_json():
+
+    tup = ("Dhirubhai", 72, True, 5.8)
+    jsonObj = json.dumps(tup)
+    print(jsonObj)
+    print(type(jsonObj))
+def json_to_tuple():
+    jsonTup = '{"Dhirubhai", 72, true, 5.8}'
+    tup = json.loads(jsonTup) # 元组字符串不能直接序列化为内置对象
+    print(tup)
+    print(type(tup))
+
 if __name__ == '__main__':
-    obj_To_Json()
+    tuple_to_json()
