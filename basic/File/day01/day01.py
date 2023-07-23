e87cdded6edaac1581e5c4a55022a66d4e194e9f
anthropologistbiubiu <1319847957@qq.com>
2023-07-23 13:47:31 +0800

    modify

--- basic/File/day01/day01.py
+++ basic/File/day01/day01.py
@@ -4,9 +4,12 @@
 # 读和写（'r+’）：这种方法以读和写模式打开文件。文件的开始是句柄所在的位置。如果文件不存在，就会产生一个 I/O 错误。
 
 def r_open():
-    f = open("myfile.txt", "r+")
-    file_name = f.name
-    print(file_name)
+    f = open("myfile.txt", "r")
+    # str = f.read()
+    strline1 = f.readlines()
+    # strline2 = f.readline()
+    print(strline1)
+    # print(strline2)
 
 def rw_open():
     f = open("myfile.txt", "r+")
@@ -31,13 +34,26 @@ def wr_open():
 
 
 def a_open():
-    f = open("myfile.txt", "r+")
-    file_name = f.name
-    print(file_name)
+    f = open("myfile.txt", "a")
+    try:
+        n = f.write(" hello world4\n")
+        print(n)
+        content = f.readlines()
+        print(content)
+    except IOError as e :
+        print(e)
 
 def aw_open():
-    f = open("myfile.txt", "r+")
-    print(n)
+    f = open("myfile.txt", "a+")
+    try:
+        n = f.write(" good study \n")
+        print(n)
+        f.close()
+        f1 = open("myfile.txt","a+")
+        content = f1.readline()  # 因为每次指针的偏移量都在最后一个职位
+        print(content)
+    except IOError as e:
+        print(e)
 
 if __name__ == '__main__':
-    wr_open()
+    aw_open()
