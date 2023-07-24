dcd8107151db3006e9098c057dbf72602c3cab63
anthropologistbiubiu <1319847957@qq.com>
2023-07-24 23:59:46 +0800

    modify

--- time/day02/day02.py
+++ time/day02/day02.py
@@ -1,11 +1,14 @@
 import datetime
-
-
+import time
 
 
 def main():
     current_time = datetime.datetime.now()
     print(current_time.strftime("%Y-%m-%d"))
+    timestamp = time.time()
+    print(timestamp)
+    utc_time = datetime.datetime.utcfromtimestamp(timestamp)
+    print(utc_time)
     pass
 
 if __name__ == '__main__':
