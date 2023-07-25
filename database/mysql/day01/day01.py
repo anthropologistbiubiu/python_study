4856ca26875fb01666187d9d438efa5bea181f14
anthropologistbiubiu <1319847957@qq.com>
2023-07-25 21:52:28 +0800

    modify

--- database/mysql/day01/day01.py
+++ database/mysql/day01/day01.py
@@ -29,6 +29,29 @@ def connect_mysql():
     pass
 
 def insert():
+    # SQL 插入语句
+    address = "127.0.0.1"
+    port = 3306
+    db = pymysql.connect(
+        host=address,  # 填写数据库的主机IP地址
+        user='root',  # 数据库用户名
+        password='',  # 数据库密码
+        port=port,  # 数据库端口号
+        database='test',  # 数据库的名称
+    )
+    cursor = db.cursor()
+
+    cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+    sql = """INSERT INTO EMPLOYEE(FIRST_NAME,
+             LAST_NAME, AGE, SEX, INCOME)
+             VALUES ('Mac', 'Mohan', 20, 'M', 2000)"""
+    try:
+        cursor.execute(sql)
+        # 提交到数据库执行
+        db.commit()
+    except:
+        # 发生错误时回滚
+        db.rollback()
     pass
 
 def drop():
---
+++ database/mysql/day02/day02.py
@@ -0,0 +1,17 @@
+import pymysql
+
+
+
+
+
+def insert():
+
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
