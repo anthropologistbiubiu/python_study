c796c9b36f9297e7178ed257c65cbff535de7c0b
anthropologistbiubiu <1319847957@qq.com>
2023-07-26 22:33:53 +0800

    modify

--- es/day01/day01.py
+++ es/day01/day01.py
@@ -7,18 +7,23 @@ from elasticsearch import Elasticsearch
 
 def main():
     try:
-        es = Elasticsearch([{"host":"localhost","port":"9200"}])
-        print(es.ping())
+        # es = Elasticsearch([{'host': '127.0.0.1', 'port': 9200}], timeout=3600)
+        es = Elasticsearch([{"host":"localhost","port":9200,"scheme":"http"}])
+        con = es.ping()
+        print(con)
     except Exception as e:
         print("Exception",e)
+        return
     if es.ping():
+        # res = es.indices.create(index='my_index1')
+        # print("es",res)
         doc = {
             'title': 'Python Elasticsearch Tutorial',
             'content': 'Elasticsearch is a powerful search engine and analytics engine.',
         }
         # 创建一个新的索引并插入文档
-        res = es.index(index='my_index', doc_type='my_type', id=1, body=doc)
-        print(res['result'])  # 输出：created
+        print(es.index(index="my_index1",doc_type='my_doc1',id=0,body=doc))
+
 
 
 if __name__ == '__main__':
