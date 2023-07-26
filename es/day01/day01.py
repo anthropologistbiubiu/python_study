ee0bf01e68e487a0c5b7b045814ca9009939f492
anthropologistbiubiu <1319847957@qq.com>
2023-07-26 21:37:48 +0800

    modify

--- es/day01/day01.py
+++ es/day01/day01.py
@@ -3,12 +3,23 @@
 from elasticsearch import Elasticsearch
 
 # 连接到本地的Elasticsearch节点
-es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
 
-# 创建一个新的索引并插入文档
-doc = {
-    'title': 'Python Elasticsearch Tutorial',
-    'content': 'Elasticsearch is a powerful search engine and analytics engine.',
-}
-res = es.index(index='my_index', doc_type='my_type', id=1, body=doc)
-print(res['result'])  # 输出：created
+
+def main():
+    try:
+        es = Elasticsearch([{"host":"localhost","port":"9200"}])
+        print(es.ping())
+    except Exception as e:
+        print("Exception",e)
+    if es.ping():
+        doc = {
+            'title': 'Python Elasticsearch Tutorial',
+            'content': 'Elasticsearch is a powerful search engine and analytics engine.',
+        }
+        # 创建一个新的索引并插入文档
+        res = es.index(index='my_index', doc_type='my_type', id=1, body=doc)
+        print(res['result'])  # 输出：created
+
+
+if __name__ == '__main__':
+    main()
