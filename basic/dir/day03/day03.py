6622534ff983f28a6dceb1b6834464c86b23c954
anthropologistbiubiu <1319847957@qq.com>
2023-12-03 17:04:09 +0800

    basic

--- basic/dir/day03/day03.py
+++ basic/dir/day03/day03.py
@@ -1,5 +1,7 @@
 
 
+
+# 选择语句
 grade = float(input())
 if grade <60:
     print("不及格")
@@ -7,3 +9,29 @@ elif grade  < 85:
     print("良好")
 else:
     print("优秀")
+
+# 循环语句1
+for item in ['sun','wei','ming']:
+    print(item)
+# 循环语句2
+for item in range (1,5,2):
+    print(item)
+
+# 循环语句3
+i = 0
+while i < 5:
+    i+=1
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
