e1b5b68445aaf7a82447be64840ef10be6898aa4
anthropologistbiubiu <1319847957@qq.com>
2023-12-07 01:39:56 +0800

    m

--- datastruct/list/day01/day01.py
+++ datastruct/list/day01/day01.py
@@ -1,17 +1,17 @@
 
 # 写一个链表的增删改查操作
 class Node:
-    def __int__(self,data=None):
+    def __init__(self,data=None):
        self.data = data
-       self.next = Node
+       self.next = None
 
 class LinkList:
-    def __int__(self):
+    def __init__(self):
         self.head = None
     # 插入节点
     def append(self,data):
         new_node = Node(data)
-        if not self.head:
+        if self.head is None:
            self.head = new_node
            return
         last_node = self.head
@@ -21,13 +21,15 @@ class LinkList:
 
     def delete(self,data):
         cur_node = self.head
+        if cur_node is None:
+            return
         # 单独处理头节点
         if cur_node.data == data:
             self.head = self.head.next
         # 处理非头节点
-        while cur_node.next.data != data:
+        while cur_node.next and cur_node.next.data != data:
             cur_node.next = cur_node.next.next
-        if cur_node is None:
+        if cur_node.next is None:
             return
         cur_node.next = cur_node.next.next
     def didsplay(self):
@@ -35,5 +37,17 @@ class LinkList:
        while cur:
            print(cur.data)
            cur = cur.next
+
+
+mylist = LinkList()
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+mylist.append(4)
+mylist.didsplay()
+mylist.delete(1)
+mylist.delete(4)
+print("delete 1")
+mylist.didsplay()
 
 
--- datastruct/list/day01/day02.py
+++ datastruct/list/day01/day02.py
@@ -1,3 +1,82 @@
-my_variable = None
-if not my_variable:
-    print("变量为空或未定义")
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next_node = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    # 插入节点
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next_node:
+            last_node = last_node.next_node
+        last_node.next_node = new_node
+
+    # 删除节点
+    def delete(self, data):
+        current_node = self.head
+        # 处理头节点
+        if current_node and current_node.data == data:
+            self.head = current_node.next_node
+            current_node = None
+            return
+
+        # 处理非头节点
+        prev_node = None
+        while current_node and current_node.data != data:
+            prev_node = current_node
+            current_node = current_node.next_node
+
+        if current_node is None:
+            return
+
+        prev_node.next_node = current_node.next_node
+        current_node = None
+
+    # 修改节点
+    def update(self, old_data, new_data):
+        current_node = self.head
+        while current_node:
+            if current_node.data == old_data:
+                current_node.data = new_data
+                return
+            current_node = current_node.next_node
+
+    # 查找节点
+    def search(self, data):
+        current_node = self.head
+        while current_node:
+            if current_node.data == data:
+                return True
+            current_node = current_node.next_node
+        return False
+
+    # 打印链表
+    def display(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.data, end=" -> ")
+            current_node = current_node.next_node
+        print("None")
+
+# 示例用法
+my_linked_list = LinkedList()
+my_linked_list.append(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.display()
+
+my_linked_list.delete(2)
+my_linked_list.display()
+
+my_linked_list.update(1, 10)
+my_linked_list.display()
+
+print(my_linked_list.search(3))  # 输出 True
+print(my_linked_list.search(5))  # 输出 False
