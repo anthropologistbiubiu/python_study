e54c3d9a51d6aab5fa4bf7da892a664e15a4a286
anthropologistbiubiu <1319847957@qq.com>
2023-12-14 21:52:12 +0800

    binaryTree

--- datastruct/Tree/day01/day01.py
+++ datastruct/Tree/day01/day01.py
@@ -4,3 +4,23 @@
 # 这里参考c++ 写二叉树的过程来写好二叉树的数据结构
 
 
+class Node:
+    def __init__(self,data=None):
+        self.data = data
+        self.left = None
+        self.right = None
+
+class BinaryTree:
+    def __init__(self):
+        self.root = None
+    def insert(self):
+        pass
+    def pre_travels(self):
+        pass
+    def inorder_travels(self):
+        pass
+    def post_orders_travels(self):
+        pass
+
+
+
