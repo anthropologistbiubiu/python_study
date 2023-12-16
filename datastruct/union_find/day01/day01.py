04fa5ba5fd2743480f5bfa55c3a91459e6eb62fe
anthropologistbiubiu <1319847957@qq.com>
2023-12-16 18:18:12 +0800

    m

--- datastruct/union_find/day01/day01.py
+++ datastruct/union_find/day01/day01.py
@@ -1,14 +1,23 @@
 # 并查集
-
-
 class DisJoinSet:
-    def __init__(self):
-        pass
+    def __init__(self,n):
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * n
     def find(self):
         pass
     def union(self):
         pass
+    def display(self):
+        pass
+
+
+
 
 if __name__ == '__main__':
     for i in range (5):
         print(i)
+
+# 集合的查找与合并,打印。
+# 如何初始化一个并查集
+
+
