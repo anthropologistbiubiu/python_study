84c85545c690e7a673aa835ddfecaa4097bb6268
anthropologistbiubiu <1319847957@qq.com>
2023-12-16 17:50:42 +0800

    m

--- datastruct/union_find/day01/day01.py
+++ datastruct/union_find/day01/day01.py
@@ -1,7 +1,14 @@
 # 并查集
 
 
+class DisJoinSet:
+    def __init__(self):
+        pass
+    def find(self):
+        pass
+    def union(self):
+        pass
 
-
-
-
+if __name__ == '__main__':
+    for i in range (5):
+        print(i)
