d1d48ce6ba43a05998a1be2426c8be036082b0be
anthropologistbiubiu <1319847957@qq.com>
2023-12-20 23:35:17 +0800

    m

--- datastruct/Tree/BRTree/03.py
+++ datastruct/Tree/BRTree/03.py
@@ -1,1 +1,211 @@
 # 这个文件主要用来写二叉树的删除
+class Node:
+    def __init__(self, key, color, left, right, parent):
+        self.key = key
+        self.color = color
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+class RedBlackTree:
+    def __init__(self):
+        self.NIL = Node(None, "BLACK", None, None, None)
+        self.root = self.NIL
+
+    def left_rotate(self, x):
+        y = x.right
+        x.right = y.left
+        if y.left != self.NIL:
+            y.left.parent = x
+        y.parent = x.parent
+        if x.parent == self.NIL:
+            self.root = y
+        elif x == x.parent.left:
+            x.parent.left = y
+        else:
+            x.parent.right = y
+        y.left = x
+        x.parent = y
+
+    def right_rotate(self, y):
+        x = y.left
+        y.left = x.right
+        if x.right != self.NIL:
+            x.right.parent = y
+        x.parent = y.parent
+        if y.parent == self.NIL:
+            self.root = x
+        elif y == y.parent.left:
+            y.parent.left = x
+        else:
+            y.parent.right = x
+        x.right = y
+        y.parent = x
+
+    def insert(self, key):
+        z = Node(key, "RED", self.NIL, self.NIL, None)
+        y = self.NIL
+        x = self.root
+
+        while x != self.NIL:
+            y = x
+            if z.key < x.key:
+                x = x.left
+            else:
+                x = x.right
+
+        z.parent = y
+        if y == self.NIL:
+            self.root = z
+        elif z.key < y.key:
+            y.left = z
+        else:
+            y.right = z
+
+        self.insert_fix(z)
+
+    def insert_fix(self, z):
+        while z.parent.color == "RED":
+            if z.parent == z.parent.parent.left:
+                y = z.parent.parent.right
+                if y.color == "RED":
+                    z.parent.color = "BLACK"
+                    y.color = "BLACK"
+                    z.parent.parent.color = "RED"
+                    z = z.parent.parent
+                else:
+                    if z == z.parent.right:
+                        z = z.parent
+                        self.left_rotate(z)
+                    z.parent.color = "BLACK"
+                    z.parent.parent.color = "RED"
+                    self.right_rotate(z.parent.parent)
+            else:
+                y = z.parent.parent.left
+                if y.color == "RED":
+                    z.parent.color = "BLACK"
+                    y.color = "BLACK"
+                    z.parent.parent.color = "RED"
+                    z = z.parent.parent
+                else:
+                    if z == z.parent.left:
+                        z = z.parent
+                        self.right_rotate(z)
+                    z.parent.color = "BLACK"
+                    z.parent.parent.color = "RED"
+                    self.left_rotate(z.parent.parent)
+
+        self.root.color = "BLACK"
+
+    def transplant(self, u, v):
+        if u.parent == self.NIL:
+            self.root = v
+        elif u == u.parent.left:
+            u.parent.left = v
+        else:
+            u.parent.right = v
+        v.parent = u.parent
+
+    def delete_node(self, key):
+        z = self.search_key(self.root, key)
+        if z == self.NIL:
+            print("Key not found in the tree.")
+            return
+
+        y = z
+        y_original_color = y.color
+        if z.left == self.NIL:
+            x = z.right
+            self.transplant(z, z.right)
+        elif z.right == self.NIL:
+            x = z.left
+            self.transplant(z, z.left)
+        else:
+            y = self.minimum(z.right)
+            y_original_color = y.color
+            x = y.right
+            if y.parent == z:
+                x.parent = y
+            else:
+                self.transplant(y, y.right)
+                y.right = z.right
+                y.right.parent = y
+            self.transplant(z, y)
+            y.left = z.left
+            y.left.parent = y
+            y.color = z.color
+
+        if y_original_color == "BLACK":
+            self.fix_delete(x)
+
+    def fix_delete(self, x):
+        while x != self.root and x.color == "BLACK":
+            if x == x.parent.left:
+                w = x.parent.right
+                if w.color == "RED":
+                    w.color = "BLACK"
+                    x.parent.color = "RED"
+                    self.left_rotate(x.parent)
+                    w = x.parent.right
+                if w.left.color == "BLACK" and w.right.color == "BLACK":
+                    w.color = "RED"
+                    x = x.parent
+                else:
+                    if w.right.color == "BLACK":
+                        w.left.color = "BLACK"
+                        w.color = "RED"
+                        self.right_rotate(w)
+                        w = x.parent.right
+                    w.color = x.parent.color
+                    x.parent.color = "BLACK"
+                    w.right.color = "BLACK"
+                    self.left_rotate(x.parent)
+                    x = self.root
+            else:
+                w = x.parent.left
+                if w.color == "RED":
+                    w.color = "BLACK"
+                    x.parent.color = "RED"
+                    self.right_rotate(x.parent)
+                    w = x.parent.left
+                if w.right.color == "BLACK" and w.left.color == "BLACK":
+                    w.color = "RED"
+                    x = x.parent
+                else:
+                    if w.left.color == "BLACK":
+                        w.right.color = "BLACK"
+                        w.color = "RED"
+                        self.left_rotate(w)
+                        w = x.parent.left
+                    w.color = x.parent.color
+                    x.parent.color = "BLACK"
+                    w.left.color = "BLACK"
+                    self.right_rotate(x.parent)
+                    x = self.root
+
+        x.color = "BLACK"
+
+    def search_key(self, node, key):
+        if node == self.NIL or key == node.key:
+            return node
+        if key < node.key:
+            return self.search_key(node.left, key)
+        return self.search_key(node.right, key)
+
+    def minimum(self, node):
+        while node.left != self.NIL:
+            node = node.left
+        return node
+
+    def inorder_traversal(self, node):
+        if node != self.NIL:
+            self.inorder_traversal(node.left)
+            print(f"Key: {node.key}, Color: {node.color}")
+            self.inorder_traversal(node.right)
+
+# 创建红黑树并插入节点
+rb_tree = RedBlackTree()
+keys_to_insert = [7, 3, 18, 10, 22, 8, 11, 26]
+for key in keys_to_insert:
+    rb_tree.insert(key)
+
