cd12a4d3ec86a04b92430101214b586b8c8cf6be
anthropologistbiubiu <1319847957@qq.com>
2023-12-25 23:33:54 +0800

    prim

--- datastruct/graph/prim.py
+++ datastruct/graph/prim.py
@@ -1,14 +1,29 @@
-
+ import heapq
 # 最小生成树算法
 
 # 给出prime 算法的
-# 1.
+# 首先从任意节点开始,把邻节点的权重都加入到一个堆中。
+# 然后从相邻节点找权重最小的边加入到最小生成树中
+# 直到把所有的节点都加入到生成树中
 
 
 
 
-def prim(graph):
-    pass
+
+
+
+def prim(graph,start):
+    visited = set()
+    visited.add(start)
+    heap = []
+    heapq.heapify(heap)
+    for neighbor ,weight in graph[start].items():
+        heapq.heappush(heap,heap)
+    while heap:
+        min_weight = heapq.heappop(heap)
+
+
+
 if __name__ == '__main__':
     prim()
 
