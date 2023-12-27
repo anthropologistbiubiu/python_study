db35a1c113f9cf47970bfa4544f85365ca719eca
anthropologistbiubiu <1319847957@qq.com>
2023-12-27 23:09:25 +0800

    m

--- basic/func/dict.py
+++ basic/func/dict.py
@@ -4,4 +4,19 @@ squares = {x: x*x for x in range(1, 6)}
 #  结果：{1: 1, 2: 4, 3: 9, 4: 16, 5: 25}
 print(squares)
 
+# 遍历字典的key
+for key in squares:
+    print(key,end=' ')
+print()
+# 遍历字典的value
+for value in squares.values():
+    print(value,end=' ')
+print()
+# 遍历字典的value
+for key,value in squares.items():
+    print(key,value,';',end='')
+print()
+
+# 遍历字典的 key,value
+
 
