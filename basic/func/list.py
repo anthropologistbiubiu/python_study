bc64e2e7185beca933aad0a1fee64518b5440040
anthropologistbiubiu <1319847957@qq.com>
2023-12-28 00:09:47 +0800

    list

--- basic/func/list.py
+++ basic/func/list.py
@@ -2,3 +2,16 @@
 my_list = [x**2 for x in range (10)]
 print(my_list)
 
+# 遍历列表
+for item in my_list:
+    print(item,end=' ')
+print()
+
+# 遍历索引和数据
+for index,item in enumerate(my_list):
+   print(index,item)
+print()
+
+# 列表解析式
+my_list1 = [(x,y) for x in range (5) for y in range (5)]
+print(my_list1)
