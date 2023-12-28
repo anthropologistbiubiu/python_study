069489f3f6a684ccb29c6a61d5d1e89e8775de3f
anthropologistbiubiu <1319847957@qq.com>
2023-12-29 00:18:53 +0800

    modules

---
+++ basic/main/day02.py
@@ -0,0 +1,12 @@
+import sys
+
+sys.path.append('/Users/biubiu/Documents/python_study')
+from mymodule.my_addition import addtion
+
+
+
+if __name__ == '__main__':
+    add_tool = addtion.Addtion(1, 2, 3, 4, 5)
+    print(add_tool.sum())
+    print(add_tool.nums)
+
