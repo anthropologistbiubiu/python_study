bb7986de0496c4638ff26a134af5ab873b321f77
anthropologistbiubiu <1319847957@qq.com>
2024-01-02 23:39:47 +0800

    flask

---
+++ flask/router/router.py
@@ -0,0 +1,20 @@
+from flask import Flask
+
+# 定义动态路由
+
+
+app = Flask(__name__)
+
+@app.route('/user/login')  # 处理json 请求
+def  login():
+    try:
+    except Exception as e:
+        pass
+    else:
+        pass
+def main():
+    app.run()
+if __name__ == '__main__':
+    main()
+
+
