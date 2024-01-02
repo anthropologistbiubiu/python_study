6dbd7a9ded622137bec7a6eb154457f0aed2d659
anthropologistbiubiu <1319847957@qq.com>
2024-01-03 02:18:44 +0800

    flask

--- flask/router/router.py
+++ flask/router/router.py
@@ -1,19 +1,23 @@
-from flask import Flask
+from flask import Flask,request,jsonify
 
 # 定义动态路由
 
 
 app = Flask(__name__)
 
-@app.route('/user/login')  # 处理json 请求
+@app.route('/user/login',methods=['POST'])  # 处理json 请求
 def  login():
     try:
+        json_data = request.get_json()
+        print(json_data)
+        response_data = {"status": "success", "message": "JSON data received successfully"}
+        return jsonify(response_data)
     except Exception as e:
-        pass
-    else:
-        pass
+        print(f"Error: {e}")
+        response_data = {"status": "error", "message": "An error occurred while processing the request"}
+        return jsonify(response_data)
 def main():
-    app.run()
+    app.run(host='127.0.0.1',port=8081,debug=True)
 if __name__ == '__main__':
     main()
 
