72243bacce01fd73f672a5bb1c12a2e2bcf7274e
anthropologistbiubiu <1319847957@qq.com>
2024-01-03 03:00:06 +0800

    flask

--- flask/router/router.py
+++ flask/router/router.py
@@ -16,6 +16,26 @@ def  login():
         print(f"Error: {e}")
         response_data = {"status": "error", "message": "An error occurred while processing the request"}
         return jsonify(response_data)
+
+@app.route('/user/payment',methods=['POST'])
+def user_payment(): # 用户使用userid 发起充值 接受一个post 表单请求。
+    try:
+        user_name = request.form.get('username')
+        password = request.form.get('username')
+        print(f'{user_name} {password}')
+        response_data = {"status": "success", "message": "JSON data received successfully"}
+        return jsonify(response_data)
+    except Exception as e:
+        print(f'Exception {e}')
+        response_data = {"status": "error", "message": "An error occurred while processing the request"}
+        return jsonify(response_data)
+
+@app.route('/user/payout',methods=['POST'])
+def user_payout():     # 用户使用userid 提现 接收一个form-data 请求
+    pass
+@app.route('/order/status',methods=['GET'])
+def get_order_status():  # 用户使用订单号查询订单状态 接收一个get 请求。
+    pass
 def main():
     app.run(host='127.0.0.1',port=8081,debug=True)
 if __name__ == '__main__':
