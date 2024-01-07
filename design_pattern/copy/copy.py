808d3eb442599e341d3c25996f2a9a0d10f27fe3
anthropologistbiubiu <1319847957@qq.com>
2024-01-08 01:09:26 +0800

    copy pattern

--- design_pattern/copy/copy.py
+++ design_pattern/copy/copy.py
@@ -1,9 +1,42 @@
+import copy # 原型模式
+
+class Circle:
+    def __init__(self,radius):
+        self.radius = radius
+    def clone(self):
+       return copy.deepcopy(self)
+    def get_config(self):
+        return self.radius
+
+class Rectangle:
+    def __init__(self,width,high):
+        self.width = width
+        self.high = high
+    def clone(self):
+       return copy.deepcopy(self)
+    def get_config(self):
+        return self.width,self.high
 
 
-# 原型模式
+class GraphEditor:
+    def __init__(self):
+        self.shapes = []
+
+    def add_shape(self, shape):
+        self.shapes.append(shape)
+
+    def print_shape(self):
+        for item in self.shapes:
+            print(type(item).__name__,item.get_config())
 
 
+def main():
+  circle = Circle(5)
+  rectangle = Rectangle(3,4)
+  edit = GraphEditor()
+  edit.add_shape(circle)
+  edit.add_shape(rectangle)
+  edit.print_shape()
 
-
-
-
+if __name__ == '__main__':
+    main()
