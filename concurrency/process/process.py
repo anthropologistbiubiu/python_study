781a71409a4443c1c6ef600dab897d5a830b9581
anthropologistbiubiu <1319847957@qq.com>
2024-01-13 15:35:33 +0800

    process

--- concurrency/process/process.py
+++ concurrency/process/process.py
@@ -1,10 +1,8 @@
 
-
 import multiprocessing
 from concurrent.futures import ProcessPoolExecutor
 
 # 进程的创建和使用
-
 def my_process(i):
     print(f'{i} my_process')
 
@@ -16,7 +14,20 @@ def my_process_pool(i):
 
 
 
+# 共享变量
+
+
+def my_function(shared_variable, lock, i):
+    with lock:
+        shared_variable.value += i
+
+
+
+# 进程的事件通知
+
+
 def main():
+
     process1 = multiprocessing.Process(target=my_process, args=(10,))
     process1.start()
     process1.join()
@@ -25,6 +36,20 @@ def main():
         for i in range(10):
             future = executor.submit(my_process_pool,i)
             print(future.result())
+
+
+    shared_variable  = multiprocessing.Value('i',0)
+    lock = multiprocessing.Lock()
+
+    process_list = [multiprocessing.Process(target=my_function,args=(shared_variable,lock,i,)) for i in range (10)]
+    for process in process_list:
+        process.start()
+    for process in process_list:
+        process.join()
+    print(shared_variable.value)
+
+
+
 
 
 if __name__ == '__main__':
