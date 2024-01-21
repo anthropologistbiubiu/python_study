8b050b40f725813d0e916f7e72fb172fbbd816c4
anthropologistbiubiu <1319847957@qq.com>
2024-01-21 23:31:44 +0800

    bit

--- basic/bit_operator/bit_div.py
+++ basic/bit_operator/bit_div.py
@@ -2,15 +2,15 @@
 
 def division(a,b):
     ans = 0
-    for i in range(64, 0, -1):
-        if a - b * (2 << i) >= 0:
-            a = a - b * (2 << i)
-            ans += (2 << i)
+    for i in range(64, -1, -1):
+        if a - b * (1 << i) >= 0:
+            a = a - b * (1 << i)
+            ans += (1 << i)
     return ans
 
 
 def main():
-   print(division(100,2))
+   print(division(100,34))
 
 if __name__ == '__main__':
     main()
