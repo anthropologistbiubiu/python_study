1bbbc2ed7e9265de4444907d47b19d94bcb1e4a4
anthropologistbiubiu <1319847957@qq.com>
2024-01-23 23:25:44 +0800

    private

--- encapsulation/var/var.py
+++ encapsulation/var/var.py
@@ -2,10 +2,17 @@
 
 
 class MyClass:
-    pass
+    def __init__(self):
+        self.__private_variable = 10
+
+    def __private_method(self):
+       print('__private_method')
+
 
 def main():
-    pass
+   obj = MyClass()
+   print(obj.__pirvate_variable)
+   obj.__private_method()
 
 if __name__ == '__main__':
     main()
