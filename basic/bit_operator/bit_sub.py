61566eb93ba94d38004dee5e387f08409a64dd50
anthropologistbiubiu <1319847957@qq.com>
2024-01-23 23:16:21 +0800

    encapsulation

--- basic/bit_operator/bit_sub.py
+++ basic/bit_operator/bit_sub.py
@@ -1,21 +1,19 @@
 
-
-
-
 def add(a, b):
     c = a ^ b
     d = (a & b) << 1
-    print(c,d)
     while d != 0:
         a = c
         b = d
         c = a ^ b
         d = (a & b) << 1
     return c
+
 # 使用位运算实现减法
 def sub(num1,num2):
-    num2 = ~num2 & 1
-    return add(num1,num2)
+    num2 = add(~num2, 1)
+    print(num2)
+    return add(num1, num2)
 
 def main():
     print(sub(10,1))
---
+++ encapsulation/var/var.py
@@ -0,0 +1,11 @@
+
+
+
+class MyClass:
+    pass
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
