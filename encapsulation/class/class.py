4417096ba06da7b45c2d5ac8d3918d390491f0f4
anthropologistbiubiu <1319847957@qq.com>
2024-01-23 23:42:16 +0800

    class

--- encapsulation/class/class.py
+++ encapsulation/class/class.py
@@ -2,12 +2,17 @@
 
 
 class MyClass:
-    pass
 
-
+    def __init__(self,name):
+        self.__name = name
+    def get_name(self):
+        return self.__name
+    def set_name(self,name):
+        self.__name = name
 
 def main():
-    pass
+    obj = MyClass('zhangsan')
+    print(obj.get_name())
 
 
 if __name__ == '__main__':
