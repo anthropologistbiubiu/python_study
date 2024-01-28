46b4017e400b9b342f1c33a02b0cdcea4dfedc1c
anthropologistbiubiu <1319847957@qq.com>
2024-01-28 22:39:11 +0800

    study

--- design_pattern/strategy/strategy.py
+++ design_pattern/strategy/strategy.py
@@ -1,31 +1,45 @@
 from abc import abstractmethod
 
+class Context:
+    def __init__(self,strategy):
+        self._strategy = strategy
+    def do_logic(self):
+        self._strategy.do_logic()
+    @property
+    def strategy(self):
+        pass
 
-
-class Context():
-    pass
-
+    @strategy.setter
+    def set_strategy(self,strategy):
+        pass
 
 class Strategy:
     @abstractmethod
     def do_logic(self):
         pass
 
-class concrecteStrategyA:
+class concrecteStrategyA(Strategy):
     def do_logic(self):
-        pass
+        print("concrecteStrategyA")
 
-class concrecteStrategyA:
+
+
+
+class concrecteStrategyB(Strategy):
     pass
-
 
 
 def client_code():
-   pass
-
+  strategyA = concrecteStrategyA()
+  context = Context(strategyA)
+  context.do_logic()
+  print()
+  strategyB = concrecteStrategyB()
+  context = Context(strategyB)
+  context.do_logic()
 
 def main():
-    pass
+   client_code()
 
 if __name__ == '__main__':
    main()
