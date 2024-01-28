f9c198ada0da4a923d8001bb75baa67e3d20b4d9
anthropologistbiubiu <1319847957@qq.com>
2024-01-28 23:35:51 +0800

    fix bug

--- design_pattern/strategy/strategy.py
+++ design_pattern/strategy/strategy.py
@@ -1,39 +1,32 @@
-from abc import abstractmethod
+from abc import ABC,abstractmethod
 
+class Strategy(ABC):
+    @abstractmethod
+    def do_logic(self):
+        pass
 class Context:
-    def __init__(self,strategy):
+    def __init__(self, strategy: Strategy):
         self._strategy = strategy
     def do_logic(self):
         self._strategy.do_logic()
     @property
     def strategy(self):
         pass
-
     @strategy.setter
     def set_strategy(self,strategy):
         pass
 
-class Strategy:
-    @abstractmethod
-    def do_logic(self):
-        pass
 
 class concrecteStrategyA(Strategy):
     def do_logic(self):
         print("concrecteStrategyA")
 
-
-
-
 class concrecteStrategyB(Strategy):
-    pass
+    def do_logic(self):
+        print("concrecteStrategyB")
 
 
 def client_code():
-  strategyA = concrecteStrategyA()
-  context = Context(strategyA)
-  context.do_logic()
-  print()
   strategyB = concrecteStrategyB()
   context = Context(strategyB)
   context.do_logic()
