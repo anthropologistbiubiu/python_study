c7b7ef315a304ff63fce6c7bee225035a1b193ff
anthropologistbiubiu <1319847957@qq.com>
2024-01-29 01:59:12 +0800

    study

--- design_pattern/responsibility/rp_chains.py
+++ design_pattern/responsibility/rp_chains.py
@@ -1,44 +1,54 @@
 from abc import ABC,abstractmethod
-
-class Handler(ABC):
-    pass
-class AbstractHandler():
-
-    _next_node: Handler = None
-    def handler(self,food):
-        pass
-
+# from __future__ import annotations
+from typing import Any
+class Handler():
     def set_next(self):
         pass
 
+class AbstractHandler():
+
+    _next_node: Handler = None
+    def set_next(self,handler:Handler) -> Handler:
+         self._next_node = handler
+         return self._next_node
+    def handler(self,food):
+        if self._next_node:
+            self._next_node.handler(food)
+        else:
+            pass
 
 class MonkeyHandler(AbstractHandler):
     def handler(self,food):
        if food == "Banana":
             print(f"monkey eat {food}")
        else:
-           super().handler()
+           super().handler(food)
 
-class SquirrelHandler():
+class SquirrelHandler(AbstractHandler):
     def handler(self,food):
         if food == "Nut":
             print(f"Squireel eat {food}")
         else:
-            super().next()
+            super().handler(food)
 
-class DogHandler():
+class DogHandler(AbstractHandler):
     def handler(self,food):
         if food == "Meatball":
             print(f"dog eat {food}")
         else:
-           super().next()
+           super().handler(food)
 
 
 def client_code():
-    pass
+    monkey = MonkeyHandler()
+    squirrel = SquirrelHandler()
+    dog = DogHandler()
+    monkey.set_next(squirrel).set_next(dog)
+    for food in ["Nut","Meatball","Banana"]:
+        monkey.handler(food)
 
 def main():
-    pass
+   client_code()
 
 if __name__ == '__main__':
     main()
