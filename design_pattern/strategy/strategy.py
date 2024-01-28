48ebf19fbaaa80ae64d0550fa32cb85a6cf1f7c3
anthropologistbiubiu <1319847957@qq.com>
2024-01-28 20:50:04 +0800

    strategy

---
+++ design_pattern/strategy/strategy.py
@@ -0,0 +1,27 @@
+
+
+
+
+class Context():
+    pass
+
+
+class Strategy:
+    pass
+class concrecteStrategyA:
+    pass
+
+class concrecteStrategyA:
+    pass
+
+
+
+def client_code():
+   pass
+
+
+def main():
+    pass
+
+if __name__ == '__main__':
+   main()
