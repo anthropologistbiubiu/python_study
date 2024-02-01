663a177851771487a2f2fb580be763021b979d60
anthropologistbiubiu <1319847957@qq.com>
2024-02-01 22:45:27 +0800

    python

--- design_pattern/observer/observer.py
+++ design_pattern/observer/observer.py
@@ -2,27 +2,34 @@ from abc import ABC, abstractmethod
 
 
 
-class AbstractSubject:
-    pass
+class AbstractSubject(ABC):
 
-class Subject(ABC):
-
-   @abstractmethod
-   def notify(self):
+    @abstractmethod
+    def notify(self):
         pass
 
-   @abstractmethod
+    @abstractmethod
+    def attach(self):
+        pass
+
+    @abstractmethod
+    def detach(self):
+        pass
+
+
+class Subject(AbstractSubject):
+
+    _observers = []
+   def notify(self):
+       pass
+
    def attach(self):
        pass
 
-   @abstractmethod
    def detach(self):
        pass
 
-
-
-
-class AbstractObserver:
+class AbstractObserver(ABC):
     pass
 
 class observerA:
