35d7897867e4b8bde96f03e84f1bee5242bf66a6
anthropologistbiubiu <1319847957@qq.com>
2024-02-04 22:51:03 +0800

    decorator

--- design_pattern/decorator/decorator.py
+++ design_pattern/decorator/decorator.py
@@ -1,23 +1,24 @@
 from abc import ABC,abstractmethod
 
 class Component:
-    pass
+    def operator(self):
+        return "Component"
 
 class Decorator(ABC):
 
+
     @abstractmethod
-    def operator(self):
+    def operator(self,component: Component):
         pass
 
 class ConcreateDecoratorA:
 
-    def operator(self):
-        pass
+    def operator(self,componemt: Component):
+        return  f"ConcreateDecoratorA + { componemt.operator() }"
 
 class ConcreateDecoratorB:
-    def operator(self):
-        pass
-
+    def operator(self,componemt: Component):
+        return  f"ConcreateDecoratorB + { componemt.operator() }"
 
 def client_code():
     pass
