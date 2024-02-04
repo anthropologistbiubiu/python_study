869de0a8a80e7d5c3aa5292c2802584008699028
anthropologistbiubiu <1319847957@qq.com>
2024-02-04 22:07:46 +0800

    decorator

---
+++ design_pattern/decorator/decorator.py
@@ -0,0 +1,23 @@
+from abc import ABC,abstractmethod
+
+class Component:
+    pass
+
+class Decorator:
+    pass
+
+
+class ConcreateDecoratorA:
+    pass
+
+class ConcreateDecoratorB:
+    pass
+
+
+def client_code():
+    pass
+
+
+if __name__ == '__main__':
+    client_code()
+
