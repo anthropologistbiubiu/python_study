c9ea4e30d5e0c9d346511b3fda1edd43dc392238
anthropologistbiubiu <1319847957@qq.com>
2024-02-05 22:54:57 +0800

    proxy

--- design_pattern/proxy/proxy.py
+++ design_pattern/proxy/proxy.py
@@ -4,22 +4,32 @@ from abc import ABC,abstractmethod
 class Subject(ABC):
 
     @abstractmethod
-    def operator(self):
+    def request(self):
         pass
 
 class RealSubject(Subject):
-    def operator(self):
-        pass
 
+    def request(self):
+        print('end real request')
 
-class proxy:
+class proxy():
+    def __init__(self, subject: RealSubject):
+        self.subject = subject
+
+    def request(self):
+        if self.access_status():
+            self.subject.request()
+        self.log_access()
+
+    def access_status(self) -> bool:
+        print(f"get access_status")
+        return True
+    def log_access(self):
+        print(f'logging the cost of request',end='')
+
+def client_code(subjectV):
     pass
 
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    client_code()
 
