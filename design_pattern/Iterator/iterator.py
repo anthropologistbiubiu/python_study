b908fffb25ff030b09b455f7e0be0b4e03622bf1
anthropologistbiubiu <1319847957@qq.com>
2024-02-06 21:43:39 +0800

    Iterator

--- design_pattern/Iterator/iterator.py
+++ design_pattern/Iterator/iterator.py
@@ -5,13 +5,19 @@ class Collection:
 
 class Iterator():
 
-   _reverse = False
-   def __init__(self,collection):
+   _pos = 0
+   def __init__(self,collection,reverse):
        self.collection = collection
+       self._reverse = reverse
 
    def __iter__(self):
-      pass
-
+       try:
+        if self._reverse:
+            value = self.collection[self._pos]
+            self._pos += 1
+            return value
+       except StopIteration as e:
+           raise e
    def get_reverse_iterator(self):
        pass
 
