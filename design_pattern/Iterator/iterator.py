965af79263e62c0ef37297154e664a3e5baccf8e
anthropologistbiubiu <1319847957@qq.com>
2024-02-06 23:33:59 +0800

    Iterator

--- design_pattern/Iterator/iterator.py
+++ design_pattern/Iterator/iterator.py
@@ -1,28 +1,50 @@
 from abc  import abstractmethod
+from collections.abc import  Iterator,Iterable
 
-class Collection:
-    pass
-
-class Iterator():
+class WordIterator(Iterator):
 
    _pos = 0
    def __init__(self,collection,reverse):
        self.collection = collection
        self._reverse = reverse
 
-   def __iter__(self):
+   def __next__(self):
        try:
-        if self._reverse:
             value = self.collection[self._pos]
-            self._pos += 1
+            if self._reverse:
+                self._pos += 1
+            else:
+                self._pos -= 1
             return value
-       except StopIteration as e:
-           raise e
-   def get_reverse_iterator(self):
-       pass
+       except  IndexError:
+           raise StopIteration()
 
+
+class WordCollection(Iterable):
+
+    def __init__(self):
+        self.datas = []
+
+    def add(self,item):
+        self.datas.append(item)
+
+    def __getitem__(self, index):
+        return self.datas[index]
+
+    def __iter__(self):
+        return WordIterator(self,True)
+
+    def get_reverse_iterator(self):
+        return WordIterator(self,False)
 def client_code():
-    pass
+    datas = WordCollection()
+    datas.add("first")
+    datas.add("second")
+    datas.add("third")
+    print("\n".join(datas))
+
+    print("\n".join(datas.get_reverse_iterator()))
+
 
 if __name__ == '__main__':
     client_code()
