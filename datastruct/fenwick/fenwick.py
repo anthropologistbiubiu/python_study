9e488912e34c6346440f35b14118650a704f2342
anthropologistbiubiu <1319847957@qq.com>
2024-03-02 22:41:58 +0800

    fenwick

--- datastruct/fenwick/fenwick.py
+++ datastruct/fenwick/fenwick.py
@@ -1,31 +1,36 @@
 
 class BinaryIndexTree:
-    def __init__(self,nums):
-        self.tree = []
-        for index,item in nums:
-            self.update(index+1,item)
+    def __init__(self, nums):
+        self.tree = [0] * (len(nums) + 1)
+        self.capacity = len(nums)
+        for i in range (self.capacity):
+            self.update(i+1, nums[i])
 
-    def update(self,index,item):
-        self.tree[index] += item
-        index += self.lowbit(index)
+    def update(self, index, item):
+        while index <= self.capacity:
+            self.tree[index] += item
+            index += self.lowbit(index)
 
-    def lowbit(self,x):
+    def lowbit(self, x):
         return x & -x
 
-    def query(self,index):
+    def query(self, index):
         total = 0
-        while index> 0:
+        while index > 0:
             total += self.tree[index]
             index -= self.lowbit(index)
-    def query_rang(self,end,start):
-        return self.query(end) - self.query(start-1)
+        return total
+
+    def query_rang(self, end, start):
+        return self.query(end) - self.query(start - 1)
 
 
 def main():
     nums = [1,3,5,7,9,10,22,5]
     bit = BinaryIndexTree(nums)
+    print(bit.tree)
     print(bit.query(2))
-    print(bit.query_rang(1,5))
+    print(bit.query_rang(5,1))
 
 
 if __name__ == '__main__':
