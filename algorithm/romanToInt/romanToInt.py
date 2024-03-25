2b88b4f7588258cd62324336774aa034645e1d08
anthropologistbiubiu <1319847957@qq.com>
2024-03-26 00:50:44 +0800

    algorithm

--- algorithm/romanToInt/romanToInt.py
+++ algorithm/romanToInt/romanToInt.py
@@ -1,7 +1,15 @@
-
-
-
 class Solution:
     def romanToInt(self, s: str) -> int:
-        pass
+        d = {"":""}
+        result = 0
+        for i, item in enumerate(s):
+            if i == 0:
+               result += d.get(s[0])
+        else:
+            result += d.get(s[i-1:i+1],d[item])
+        return result
+
+
+
 solution = Solution()
+print(solution.romanToInt("IV"))
