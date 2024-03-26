55bc58572c0fbd37708183111038b62466d6ab95
anthropologistbiubiu <1319847957@qq.com>
2024-03-26 22:16:53 +0800

    finish

--- algorithm/bracket/isValid.py
+++ algorithm/bracket/isValid.py
@@ -1,4 +1,24 @@
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        pass
+        stack = []
+        dic = {"{": "}", "(": ")", "[": "]"}
+        for i in s:
+            if i in ['(','[','{']:
+                stack.append(i)
+            else:
+                if len(stack) == 0:
+                    return False
+                left = stack.pop()
+                if i != dic[left]:
+                    return False
+        if len(stack) > 0:
+            return False
+        return True
+
+
+
+solution = Solution()
+
+print(solution.isValid("]"))
+
