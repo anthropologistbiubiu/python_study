03431178b21382c2642c86245a59d63b9f7110a7
anthropologistbiubiu <1319847957@qq.com>
2024-03-28 23:55:15 +0800

    remove Element

--- algorithm/removeElement/removeElement.py
+++ algorithm/removeElement/removeElement.py
@@ -1,6 +1,19 @@
 
+from typing import List
 
 
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        if len(nums) == 0:
+            return 0
+        left, right = 0, len(nums) - 1
+        while left < right:
+            if nums[left] != val:
+                left += 1
+            else:
+                nums[left] = nums[right]
+
+
+solution = Solution()
+print(solution.removeElement([1,2,3,4,5],5))
