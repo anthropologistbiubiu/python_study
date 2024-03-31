8ada1a7b5ccb97fff68d80d94466fd95effce835
anthropologistbiubiu <1319847957@qq.com>
2024-03-31 15:06:58 +0800

    dataStruct

--- datastruct/Tree/traverse/inOrder.py
+++ datastruct/Tree/traverse/inOrder.py
@@ -9,7 +9,38 @@ class TreeNode:
          self.right = right
 class Solution:
     def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        res = []
+        self.preOrder(root,res)
+        return res
+    def preOrder(self,root,res):
+        if not root:
+            return
+        res.append(root.val)
+        self.preOrde(root.left,res)
+        self.preOrde(root.right,res)
 
-        pass
+
+
+
+
+
+class Solution1:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        result = []
+        self.inorder(root, result)
+        return result
+
+    def inorder(self, node, result):
+        if node:
+            self.inorder(node.left, result)
+            result.append(node.val)
+            self.inorder(node.right, result)
+
+
+solution = Solution1()
+print(solution.inorderTraversal([1,3,2]))
+
+
+
 
 
