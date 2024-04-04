a58e62fce6b197ad277a057150c23d0fa78f98d9
anthropologistbiubiu <1319847957@qq.com>
2024-04-04 20:27:21 +0800

    isSameTree

--- datastruct/Tree/isSame/isSameTree.py
+++ datastruct/Tree/isSame/isSameTree.py
@@ -12,6 +12,11 @@ class Solution:
             return False
         elif not q and p:
             return False
+        elif not q and not p:
+            return True
         elif q.val != p.val:
            return False
-        return self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        result = self.isSameTree(p.left,q.left) and self.isSameTree(p.right,q.right)
+        if not result:
+            return result
+        return True
