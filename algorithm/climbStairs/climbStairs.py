1b8d6fb87c8a6333967de9fd4446e2920b3c4341
anthropologistbiubiu <1319847957@qq.com>
2024-04-08 23:31:35 +0800

    study

--- algorithm/climbStairs/climbStairs.py
+++ algorithm/climbStairs/climbStairs.py
@@ -1,9 +1,19 @@
 
 
-
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dp = []
-        for i in range (n):
-          if i == 0:
-            pass
+        dp = [0] * (n+1)
+        for i in range(len(dp)):
+            print(i)
+            if i == 0 or i == 1:
+                dp[i] = 1
+            elif i == 2:
+                dp[i] = 2
+            else:
+                dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
+
+
+solution = Solution()
+print(solution.climbStairs(3))
+
