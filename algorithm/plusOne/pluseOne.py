2d4dc21a6faa95923b8f96aac8753bd465eeb613
anthropologistbiubiu <1319847957@qq.com>
2024-04-09 22:31:06 +0800

    study

--- algorithm/plusOne/pluseOne.py
+++ algorithm/plusOne/pluseOne.py
@@ -2,5 +2,17 @@ from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+
         for i in range(len(digits)-1,-1,-1):
-            pass
+            if i != 9:
+                digits[i] +=1
+                return digits
+            else:
+                for j in range (i,len(digits),1):
+                    digits[j] = 0
+
+        return digits
+
+
+solution = Solution()
+print(solution.plusOne([1,2,9]))
