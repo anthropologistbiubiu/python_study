cfe72155dc25d05f05762114696086dca86b9831
anthropologistbiubiu <1319847957@qq.com>
2024-04-09 22:56:44 +0800

    study

--- algorithm/plusOne/pluseOne.py
+++ algorithm/plusOne/pluseOne.py
@@ -4,15 +4,17 @@ class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
 
         for i in range(len(digits)-1,-1,-1):
-            if i != 9:
+            print(i)
+            if digits[i] != 9:
                 digits[i] +=1
                 return digits
             else:
-                for j in range (i,len(digits),1):
-                    digits[j] = 0
-
+                digits[i] = 0
+            if i == 0:
+                digits.append(0)
+                digits[0] = 1
         return digits
 
 
 solution = Solution()
-print(solution.plusOne([1,2,9]))
+print(solution.plusOne([9,8,9]))
