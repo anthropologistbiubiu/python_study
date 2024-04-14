b911de06d99e9e138f89845e0d0a1c266de83e87
anthropologistbiubiu <1319847957@qq.com>
2024-04-14 18:01:43 +0800

    algorithm

--- algorithm/triangle/yanghui.py
+++ algorithm/triangle/yanghui.py
@@ -2,6 +2,24 @@
 from typing import List
 
 
-class Solution:
+class Solution1:
     def generate(self, numRows: int) -> List[List[int]]:
+        ans = []
+        print(ans)
+        for i in range(numRows):
+            rows = [0] * (i+1)
+            for j in range(i+1):
+                if j == 0 or j == i:
+                    rows[j] = 1
+                else:
+                    rows[j] = ans[i-1][j-1] + ans[i-1][j]
+            ans.append(rows)
+        return ans
+
+class Solution2:
+    def getRow(self, rowIndex: int) -> List[int]:
         pass
+
+solution = Solution2()
+print(solution.generate(5))
+
