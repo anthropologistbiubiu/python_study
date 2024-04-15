eedc4ffc83102d53608edf22e796a38bfe26489a
anthropologistbiubiu <1319847957@qq.com>
2024-04-15 21:59:33 +0800

    isBalanced

--- algorithm/triangle/yanghui.py
+++ algorithm/triangle/yanghui.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 class Solution1:
     def generate(self, numRows: int) -> List[List[int]]:
         ans = []
@@ -16,10 +15,22 @@ class Solution1:
         return ans
 
 
+
 class Solution2:
     def getRow(self, rowIndex: int) -> List[int]:
         ans = []
+        for i in range(rowIndex+1):
+            rows = [0] * (i+1)
+            for j in range(i+1):
+                if j == 0 or j == i:
+                    rows[j] = 1
+                else:
+                    rows[j] = ans[i-1][j-1] + ans[i-1][j]
+            ans.append(rows)
+        return ans[rowIndex]
+
+
 
 solution = Solution2()
-print(solution.generate(5))
+print(solution.getRow(3))
 
