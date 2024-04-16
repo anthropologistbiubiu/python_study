00e3a41946b6d11707987525be70be2ed0887009
anthropologistbiubiu <1319847957@qq.com>
2024-04-16 23:13:45 +0800

    study

--- algorithm/removeDuplicateList/remove.py
+++ algorithm/removeDuplicateList/remove.py
@@ -7,4 +7,12 @@ class ListNode:
          self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        pass
+       if not head:
+           return head
+       pre = head
+       while pre.next:
+            if pre.val == pre.next.val:
+                pre.next = pre.next.next
+            else:
+                pre = pre.next
+       return head
