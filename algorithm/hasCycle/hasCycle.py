5fbe6faaefbcf8ba2ff7904a0664029a985e0238
anthropologistbiubiu <1319847957@qq.com>
2024-04-18 23:14:14 +0800

    study

--- algorithm/hasCycle/hasCycle.py
+++ algorithm/hasCycle/hasCycle.py
@@ -10,4 +10,12 @@ class ListNode:
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
         if not head:
-            return
+            return False
+        fast = head
+        slow = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+            if slow == fast:
+                return True
+        return False
