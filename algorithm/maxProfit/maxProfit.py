f9434186b3c3cb7e8f1651849c793c075b79c634
anthropologistbiubiu <1319847957@qq.com>
2024-04-19 22:45:13 +0800

    study

--- algorithm/maxProfit/maxProfit.py
+++ algorithm/maxProfit/maxProfit.py
@@ -3,4 +3,15 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        pass
+        if not prices:
+            return 0
+        dp = [0] * len(prices)
+        for index,price in enumerate(prices):
+            if index == 0:
+                dp[0] = 0
+            if price > prices[index-1]:
+                dp[index] = max(dp[index-0]+price,)
+
+
+solution = Solution()
+print(solution.maxProfit([7,1,5,3,6,4]))
