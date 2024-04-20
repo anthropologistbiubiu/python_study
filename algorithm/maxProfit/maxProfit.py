7e144505c52e7c563ba09d82d2193b2fcaaf485a
anthropologistbiubiu <1319847957@qq.com>
2024-04-20 15:19:00 +0800

    permute

--- algorithm/maxProfit/maxProfit.py
+++ algorithm/maxProfit/maxProfit.py
@@ -6,12 +6,20 @@ class Solution:
         if not prices:
             return 0
         dp = [0] * len(prices)
-        for index,price in enumerate(prices):
+        min = 0
+        for index, price in enumerate(prices):
             if index == 0:
                 dp[0] = 0
-            if price > prices[index-1]:
-                dp[index] = max(dp[index-0]+price,)
-
+            else:
+                if price > prices[index - 1]:
+                    dp[index] = max(dp[index-1],prices[index] - prices[min])
+                else:
+                    dp[index] = dp[index-1]
+                    if prices[index] < prices[min]:
+                        min = index
+        return dp[len(prices) - 1]
 
 solution = Solution()
-print(solution.maxProfit([7,1,5,3,6,4]))
+# print(solution.maxProfit([7,1,5,3,6,4]))
+print(solution.maxProfit([3,2,6,5,0,3]))
+
