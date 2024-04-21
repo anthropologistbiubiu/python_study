6d5118d175aff87d19a0ced54d23937bbb1766f8
anthropologistbiubiu <1319847957@qq.com>
2024-04-21 11:24:06 +0800

    study

---
+++ algorithm/permutation.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+
+# 无重复字符串的排列组合
+# https://leetcode.cn/problems/permutation-i-lcci/
+
+# 有重复字符串的排列组合
+# https://leetcode.cn/problems/permutation-ii-lcci/description/
+class Solution:
+    def permutation(self, S: str) -> List[str]:
+        pass
+
+    def permutate(self, S: str) -> List[str]:
+        pass
+
+
+
+solution = Solution()
+solution.permutation()
