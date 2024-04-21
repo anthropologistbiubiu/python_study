1b7a35a3a3ccae700b4321298ed3bcfae356b238
anthropologistbiubiu <1319847957@qq.com>
2024-04-21 16:18:40 +0800

    algorithm

--- algorithm/permutation.py
+++ algorithm/permutation.py
@@ -9,12 +9,27 @@ from typing import List
 # https://leetcode.cn/problems/permutation-ii-lcci/description/
 class Solution:
     def permutation(self, S: str) -> List[str]:
-        pass
-
+        ans = []
+        s = list(S)
+        self.dfs(s, 0, ans)
+        return ans
     def permutate(self, S: str) -> List[str]:
         pass
+    def dfs(self,s,cur,ans :list):
+        if cur == len(s):
+            ans.append(''.join(s))
+            return
+        mset = set()
+        for i in range(cur,len(s)):
+            if s[i] in mset:
+                continue
+            mset.add(s[i])
+            s[i], s[cur] = s[cur], s[i]
+            self.dfs(s, cur + 1, ans)
+            s[i], s[cur] = s[cur], s[i]
+
 
 
 
 solution = Solution()
-solution.permutation()
+print(solution.permutation("ab"))
