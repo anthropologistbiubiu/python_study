614a1f653d91f16a67982438f69153431215ae37
anthropologistbiubiu <1319847957@qq.com>
2024-04-25 23:20:20 +0800

    study

--- algorithm/IntersectionNode/intersectionNode.py
+++ algorithm/IntersectionNode/intersectionNode.py
@@ -6,12 +6,18 @@ class ListNode:
 
 class Solution:
     def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
-        lenghtA = 0
+        lengthA  = 0
         lenghtB = 0
         while headA:
             headA = headA.next
-            lenghtA+=1
+            lengthA += 1
         while headB:
             headB = headB.next
             lenghtB += 1
-        pass
+        fast = headA if lengthA > lenghtB else headB
+        slow = headA if lengthA < lenghtB else headB
+        long = lengthA if lengthA > lenghtB else lenghtB
+        short = lengthA if lengthA < lenghtB else lenghtB
+        while short < long:
+            fast = fast.next
+            short += 1
