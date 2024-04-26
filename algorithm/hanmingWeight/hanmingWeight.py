788f6eda8dfa66a13b9aba2c3a7daab9334d7a4d
anthropologistbiubiu <1319847957@qq.com>
2024-04-26 23:15:02 +0800

    study

--- algorithm/hanmingWeight/hanmingWeight.py
+++ algorithm/hanmingWeight/hanmingWeight.py
@@ -2,13 +2,13 @@
 
 
 
-def hammingWeight(self, n: int) -> int:
+def hammingWeight(n: int) -> int:
     count = 0
     while n>0:
         if n & 1 == 1:
             count += 1
-        n>>1
-
+        n = n>>1
+    return count
 
 
 
