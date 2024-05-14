47a7ffdf6ccfd7fc05ec8acf057c789bf8b45c59
anthropologistbiubiu <1319847957@qq.com>
2024-05-14 22:47:32 +0800

    study

--- algorithm/reverseBits/reverseBits.py
+++ algorithm/reverseBits/reverseBits.py
@@ -1,4 +1,14 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
+        result = 0
         for i in range(0,32):
-             pass
+            if n & 1 == 1:
+                result = result | 1
+            result = result << 1
+            n = n >> 1
+        return result
+
+
+
+solution = Solution()
+print(solution.reverseBits(43261596))
