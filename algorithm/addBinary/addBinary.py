255692f4a62aeeef9b1e952180a6c0399e534d13
anthropologistbiubiu <1319847957@qq.com>
2024-05-15 21:19:25 +0800

    study

--- algorithm/addBinary/addBinary.py
+++ algorithm/addBinary/addBinary.py
@@ -2,10 +2,26 @@
 
 
 class Solution:
-    def addBinary(self, a: str, b: str) -> str:
-        if len(a) ==0:
-
-            pass
-
+        def addBinary(self, a: str, b: str) -> str:
+            result = ''
+            carry = 0
+            index = 0
+            a = a[::-1]
+            b = b[::-1]
+            while index < len(a) and index < len(b):
+                result = str((carry + int(a[index]) + int(b[index])) % 2) + result
+                carry = (carry + int(a[index]) + int(b[index])) // 2
+                index += 1
+            while index < len(a):
+                result = str((carry + int(a[index])) % 2) + result
+                carry = (carry + int(a[index])) // 2
+                index += 1
+            while index < len(b):
+                result = str((carry + int(b[index])) % 2) + result
+                carry = (carry + int(b[index])) // 2
+                index += 1
+            if carry == 1:
+                result = str(carry) + result
+            return result
 
 
