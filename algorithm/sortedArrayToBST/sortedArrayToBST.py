0dab48e24eefa49ddece0a4fafd8808f15cecfac
anthropologistbiubiu <1319847957@qq.com>
2024-05-19 12:28:25 +0800

    study

--- algorithm/sortedArrayToBST/sortedArrayToBST.py
+++ algorithm/sortedArrayToBST/sortedArrayToBST.py
@@ -5,9 +5,15 @@ class TreeNode:
         self.val = val
         self.left = left
         self.right = right
+
 class Solution:
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
         if len(nums) == 0:
             return None
-        else:
-            pass
+        value = nums[len(nums)] // 2
+        node = TreeNode(value,None,None)
+        value.left = self.sortedArrayToBST(nums)
+        value.right = self.sortedArrayToBST(nums)
+
+solution = Solution()
+solution.sortedArrayToBST([1,2,3,4,5,6])
