a2a89a01e2d9912d6da0dea34b24a98c978fe047
anthropologistbiubiu <1319847957@qq.com>
2024-05-19 14:31:05 +0800

    study

--- algorithm/longestCommonPrefix/longestCommonPrefix.py
+++ algorithm/longestCommonPrefix/longestCommonPrefix.py
@@ -1,12 +1,22 @@
 from typing import List
 
 
-
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        pass
+        if len(strs) == 0:
+            return ""
+        shortest = strs[0]
+        for i in range(0,len(strs)):
+            if len(strs[i]) < len(shortest):
+                shortest = strs[i]
+        for i in range (0,len(shortest)):
+            for j in range (0,len(strs)):
+                if strs[j][:i] != shortest[:i]:
+                    return shortest[:i]
+        return ""
 
 
-
-
+strs = ["flower","flow","flight"]
+solution = Solution()
+print(solution.longestCommonPrefix(strs))
 
