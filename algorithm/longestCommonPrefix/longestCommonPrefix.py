d78ab8d657af8877e2c840ab73688a218541730a
anthropologistbiubiu <1319847957@qq.com>
2024-05-19 15:25:59 +0800

    study

--- algorithm/longestCommonPrefix/longestCommonPrefix.py
+++ algorithm/longestCommonPrefix/longestCommonPrefix.py
@@ -5,18 +5,30 @@ class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         if len(strs) == 0:
             return ""
-        shortest = strs[0]
-        for i in range(0,len(strs)):
-            if len(strs[i]) < len(shortest):
-                shortest = strs[i]
+        shortest = min(strs,key=len)
         for i in range (0,len(shortest)):
             for j in range (0,len(strs)):
-                if strs[j][:i] != shortest[:i]:
+                if strs[j][i] != shortest[i]:
                     return shortest[:i]
-        return ""
+        return shortest
+    def longestCommonPrefix1(self, strs: List[str]) -> str:
+        if not strs:
+            return ""
+
+        # 找到最短的字符串
+        shortest = min(strs, key=len)
+
+        for i in range(len(shortest)):
+            for string in strs:
+                if string[i] != shortest[i]:
+                    return shortest[:i]
+
+        return shortest
 
 
-strs = ["flower","flow","flight"]
+# strs = ["flower","flow","flight"]
+#strs = ["dog","racecar","car"]
+strs = ["a","b"]
 solution = Solution()
 print(solution.longestCommonPrefix(strs))
 
