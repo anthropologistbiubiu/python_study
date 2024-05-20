03deebed3a8624977d8c6192bc842cc37b9dc0da
anthropologistbiubiu <1319847957@qq.com>
2024-05-20 23:37:33 +0800

    algorithm

--- algorithm/strMatch/strMathch.py
+++ algorithm/strMatch/strMathch.py
@@ -1,23 +1,49 @@
-
 class Solution:
+    def compute_next(self,pattern) -> [int]:
+        i = 0
+        j = -1
+        next = [-1] * len(pattern)
+        while i < len(pattern) - 1:
+            if j == -1 or pattern[i] == pattern[j]:
+                i += 1
+                j += 1
+                next[i] = j
+            else:
+                j = next[j]
+        return next
+
     def strStr(self, haystack: str, needle: str) -> int:
-        pass
+
+        n = len(haystack)
+        m = len(needle)
+        i = 0
+        j = 0
+        next_array = self.compute_next(needle)
+        while i < n and j < m:
+            if j == -1 or haystack[i] == needle[j]:
+                i += 1
+                j += 1
+            else:
+                j = next_array[j]
+        if j == m:
+            return i - j
+        else:
+            return -1
+
+
+
 
 '''
-
 next数组的每个元素表示模式串中当前位置之前的子串的前缀和后缀的最长相等长度。
 具体地，next[i]表示模式串的第0到第i-1个字符的子串中，最长的相等前缀和后缀的长度。
-
 '''
-def compute_next(pattern):
-    return None
 
 # 示例
-pattern = "abababca"
-next_array = compute_next(pattern)
-print(f"Next array: {next_array}")
+pattern = "ABABCABAB"
+text = "ABABDABACDABABCABAB"
 solution = Solution()
-print(solution.strStr())
+print(solution.compute_next(pattern))
+print(solution.strStr(text,pattern))
 
 
 
