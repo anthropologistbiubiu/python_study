8c6cff9f4552dca4d85ef4fd70ba49f2797dcdf0
anthropologistbiubiu <1319847957@qq.com>
2024-06-29 15:43:21 +0800

    study

--- algorithm/lengthOfLongestSubString/lengthOfLongestSubstring.py
+++ algorithm/lengthOfLongestSubString/lengthOfLongestSubstring.py
@@ -1,15 +1,17 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        set = set()
+        chars = set()
         left, right = 0, 0
         maxLen = 0
         while left < right:
-            if s[right] not in set:
+            if s[right] not in chars:
                 right += 1
-                set.add(s[right])
+                chars.add(s[right])
+                maxLen = max(maxLen,right-left+1)
             else:
-                if s[left] in set:
+                if s[left] in chars:
                     left += 1
+                    chars.remove(s[left])
         return maxLen
 
 solution = Solution()
