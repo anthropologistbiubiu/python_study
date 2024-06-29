32f0652871c990d3130134dcaf3f73eff0f3fd2a
anthropologistbiubiu <1319847957@qq.com>
2024-06-29 19:15:44 +0800

    study

--- algorithm/longestPalindrome/longestPalindrome.py
+++ algorithm/longestPalindrome/longestPalindrome.py
@@ -1,22 +1,38 @@
-
-
 # 最长回文字串
 class Solution:
     def longestPalindrome(self, s: str) -> str:
 
         if len(s) < 2:
             return s
-        dp = [[0]*(len(s)+1)]*(len(s)+1)
-        for i in range(0,len(s)):
+        dp = [[False] * len(s) for _ in range(len(s))]
+        # dp = [[False] * (len(s))] * (len(s))
+        maxLen = 0
+        ans = ""
+        for i in range(0, len(s)):
             dp[i][i] = True
-        for i in range(0,len(s)-1):
-            if s[i] == s[i+1]:
-                dp[i][i+1] = True
+        for i in range(0, len(s) - 1):
+            if s[i] == s[i + 1]:
+                dp[i][i + 1] = True
             else:
-                dp[i][i+1] = False
-        for i in range(3,len(s)-1):
-            pass
-
+                dp[i][i + 1] = False
+        for length in range(3, len(s)):
+            for i in range(0, len(s) - length):
+                j = i + length -1
+                if dp[i + 1][j- 1] and s[i] == s[j]:
+                    dp[i][i + length] = True
+                else:
+                    dp[i][i + length] = False
+        print(dp)
+        '''
+        for i in range(0, len(s)):
+            for j in range(i, len(s)):
+                if dp[i][j] and j-i > maxLen:
+                    maxLen = j-i + 1
+                    ans = s[i:j+1]
+                    print(i,j)
+        '''
+        return ans
 
 solution = Solution()
-solution.longestPalindrome("SSSS")
+print(solution.longestPalindrome("cbba"))
+print(solution.longestPalindrome("babad"))
