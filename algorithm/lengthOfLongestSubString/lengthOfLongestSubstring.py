dd13e34b2aa0c63b1bea12fffcb91141e6b290ab
anthropologistbiubiu <1319847957@qq.com>
2024-06-29 15:23:45 +0800

    study

--- algorithm/lengthOfLongestSubString/lengthOfLongestSubstring.py
+++ algorithm/lengthOfLongestSubString/lengthOfLongestSubstring.py
@@ -1,3 +1,9 @@
 class Solution:
-    def lengthOfLongestSubstring(self, s: str) -> int:
-        pass
+    class Solution:
+
+        def lengthOfLongestSubstring(self, s: str) -> int:
+            set = set()
+            left, right = 0, 0
+            while left < right:
+                pass
+            pass
