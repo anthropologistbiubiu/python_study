57cd6abfcf231c1daada402868a5ebf07654b42e
anthropologistbiubiu <1319847957@qq.com>
2024-06-29 17:59:58 +0800

    study

--- algorithm/lengthOfLongestSubString/lengthOfLongestSubstring.py
+++ algorithm/lengthOfLongestSubString/lengthOfLongestSubstring.py
@@ -3,16 +3,14 @@ class Solution:
         chars = set()
         left, right = 0, 0
         maxLen = 0
-        while left < right:
+        while right < len(s):
             if s[right] not in chars:
-                right += 1
                 chars.add(s[right])
                 maxLen = max(maxLen,right-left+1)
+                right += 1
             else:
                 if s[left] in chars:
-                    left += 1
                     chars.remove(s[left])
+                    left += 1
         return maxLen
 
-solution = Solution()
-print(solution.lengthOfLongestSubstring("abcaaaa"))
--- algorithm/longestPalindrome/longestPalindrome.py
+++ algorithm/longestPalindrome/longestPalindrome.py
@@ -1,4 +1,22 @@
+
+
+# 最长回文字串
 class Solution:
     def longestPalindrome(self, s: str) -> str:
 
-        pass
+        if len(s) < 2:
+            return s
+        dp = [[0]*(len(s)+1)]*(len(s)+1)
+        for i in range(0,len(s)):
+            dp[i][i] = True
+        for i in range(0,len(s)-1):
+            if s[i] == s[i+1]:
+                dp[i][i+1] = True
+            else:
+                dp[i][i+1] = False
+        for i in range(3,len(s)-1):
+            pass
+
+
+solution = Solution()
+solution.longestPalindrome("SSSS")
