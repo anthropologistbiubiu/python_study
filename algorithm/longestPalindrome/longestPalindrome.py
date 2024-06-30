2247b91d4ae9de91657985cf59d2bffcf0bb4d63
anthropologistbiubiu <1319847957@qq.com>
2024-06-30 10:51:13 +0800

    study

--- algorithm/longestPalindrome/longestPalindrome.py
+++ algorithm/longestPalindrome/longestPalindrome.py
@@ -7,22 +7,27 @@ class Solution:
         dp = [[False] * len(s) for _ in range(len(s))]
         # dp = [[False] * (len(s))] * (len(s))
         maxLen = 0
-        ans = ""
+        st = 0
         for i in range(0, len(s)):
             dp[i][i] = True
+            st = i
+            maxLen = 1
         for i in range(0, len(s) - 1):
             if s[i] == s[i + 1]:
                 dp[i][i + 1] = True
+                st = i
+                maxLen = 2
             else:
                 dp[i][i + 1] = False
-        for length in range(3, len(s)):
-            for i in range(0, len(s) - length):
-                j = i + length -1
-                if dp[i + 1][j- 1] and s[i] == s[j]:
-                    dp[i][i + length] = True
+        for length in range(3, len(s)+1):
+            for i in range(0, len(s) - length + 1):
+                j = i + length - 1
+                if dp[i + 1][j - 1] and s[i] == s[j]:
+                    dp[i][j] = True
+                    st = i
+                    maxLen = length
                 else:
-                    dp[i][i + length] = False
-        print(dp)
+                    dp[i][j] = False
         '''
         for i in range(0, len(s)):
             for j in range(i, len(s)):
@@ -31,8 +36,11 @@ class Solution:
                     ans = s[i:j+1]
                     print(i,j)
         '''
-        return ans
+        return s[st:st+maxLen]
+
 
 solution = Solution()
 print(solution.longestPalindrome("cbba"))
 print(solution.longestPalindrome("babad"))
+print(solution.longestPalindrome("ccc"))
+print(solution.longestPalindrome("ccd"))
