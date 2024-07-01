50f79b7cdba0222cbb14e75ccf1c1ca3681c9dce
anthropologistbiubiu <1319847957@qq.com>
2024-07-01 23:46:16 +0800

    study

--- algorithm/reverse/reverse.py
+++ algorithm/reverse/reverse.py
@@ -13,10 +13,8 @@ class Solution:
             pop = y % 10
             y = y // 10
             if result > INT_MAX // 10 or (result == INT_MAX // 10 and pop > 7):
-                print('XXXX',result,pop,INT_MAX)
                 return 0
             if result < INT_MIN // 10 or (result == INT_MIN // 10 and pop < -8):
-                print('|||||',result,pop)
                 return 0
             result = result * 10 + pop
         if x < 0:
