581063b35619ac8b234d4b61dbc1931aa2cc18de
anthropologistbiubiu <1319847957@qq.com>
2024-07-01 22:30:39 +0800

    study

--- algorithm/reverse/reverse.py
+++ algorithm/reverse/reverse.py
@@ -5,4 +5,15 @@
 
 class Solution:
     def reverse(self, x: int) -> int:
-        pass
+        result = 0
+        pop = 0
+        while x != 0:
+            pop = x % 10
+            x = x // 10
+            result = result * 10 + pop
+        return result
+
+
+
+solution = Solution()
+print(solution.reverse(321))
