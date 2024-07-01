1109075dcd0ad7a4a7dedd37db317d5505f22a95
anthropologistbiubiu <1319847957@qq.com>
2024-07-01 23:17:46 +0800

    study

--- algorithm/reverse/reverse.py
+++ algorithm/reverse/reverse.py
@@ -5,15 +5,25 @@
 
 class Solution:
     def reverse(self, x: int) -> int:
+        y = abs(x)
         result = 0
-        pop = 0
-        while x != 0:
-            pop = x % 10
-            x = x // 10
+        INT_MAX = 2 ** 31 - 1
+        INT_MIN = -2 ** 31
+        while y != 0:
+            pop = y % 10
+            y = y // 10
+            if result > INT_MAX // 10 or (result == INT_MAX // 10 and pop > 7):
+                print('XXXX',result,pop,INT_MAX)
+                return 0
+            if result < INT_MIN // 10 or (result == INT_MIN // 10 and pop < -8):
+                print('|||||',result,pop)
+                return 0
             result = result * 10 + pop
+        if x < 0:
+            return -result
         return result
 
 
 
 solution = Solution()
-print(solution.reverse(321))
+print(solution.reverse(-321))
