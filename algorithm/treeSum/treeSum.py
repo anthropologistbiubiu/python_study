212f36957d35ac4fa812312d08c11458964c7223
anthropologistbiubiu <1319847957@qq.com>
2024-07-07 00:11:11 +0800

    study

--- algorithm/treeSum/treeSum.py
+++ algorithm/treeSum/treeSum.py
@@ -5,15 +5,26 @@ class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
         ans = []
+        if len(nums) == 3 and nums[0] + nums[1] + nums[2] == 0:
+             ans.append([nums[0],nums[1],nums[2]])
         for i in range(0,len(nums)-3):
             left = i + 1
             right = len(nums) -1
             while left < right:
                 if nums[i] > 0:
                     break
-                if nums[i] + nums[left] + nums[right] == 0:
-                    ans.append([nums[i], nums[left], nums[right]])
+                if nums[i] == nums[i-1] and i > 0:
                     break
+                if nums[i] + nums[left] + nums[right] == 0:
+                    print(i,nums[i])
+                    ans.append([nums[i], nums[left], nums[right]])
+                    left_value = nums[left]
+                    right_value = nums[right]
+                    # -4, -1, -1, 0, 1, 2))
+                    while left < right and left_value == nums[left]:
+                        left += 1
+                    while left < right and right_value == nums[right]:
+                        right -= 1
                 elif nums[i] + nums[left] + nums[right] > 0:
                     right -= 1
                 elif nums[i] + nums[left] + nums[right] < 0:
@@ -26,9 +37,10 @@ class Solution:
 
 solution = Solution()
 print(solution.threeSum([-1,0,1,2,-1,-4]))
-print(solution.threeSum([0,0,0]))
-print(solution.threeSum([0,1,1]))
-print(solution.threeSum([0,0]))
+
+# print(solution.threeSum([0,0,0]))
+# print(solution.threeSum([0,1,1]))
+# print(solution.threeSum([0,0]))
 
 '''
 输入：nums = [0,1,1]
