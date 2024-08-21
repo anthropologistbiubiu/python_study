8ef2dc3e9fcd327e10d12459b524bc229c8542c4
anthropologistbiubiu <1319847957@qq.com>
2024-08-21 22:49:48 +0800

    study

--- algorithm/convertToTitle/convertToTitle.py
+++ algorithm/convertToTitle/convertToTitle.py
@@ -1,4 +1,23 @@
 
+
 class Solution:
     def convertToTitle(self, columnNumber: int) -> str:
-        pass
+        result = ""
+        while columnNumber > 0:
+            a = columnNumber // 26
+            b = columnNumber % 26
+            columnNumber = a
+            c = str(b)
+            result = result + c
+        return result
+
+
+
+
+
+
+
+
+print(28//26)
+print(28 % 26)
+
