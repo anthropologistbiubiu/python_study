b8460948128b86025d05b3286eb9e585d300d5d7
anthropologistbiubiu <1319847957@qq.com>
2024-08-25 14:08:07 +0800

    study

--- algorithm/titleToNumber/titleToNumber.py
+++ algorithm/titleToNumber/titleToNumber.py
@@ -1,14 +1,45 @@
 
 
-
 class Solution:
     def titleToNumber(self, columnTitle: str) -> int:
         result = 0
         map = {
-            0:"Z",1: 'A', 2: 'B', 3: 'C', 4: 'D', 5: 'E', 6: 'F', 7: 'G', 8: 'H', 9: 'I',
-            10: 'J', 11: 'K', 12: 'L', 13: 'M', 14: 'N', 15: 'O', 16: 'P', 17: 'Q', 18: 'R',
-            19: 'S', 20: 'T', 21: 'U', 22: 'V', 23: 'W', 24: 'X', 25: 'Y', 26: 'Z'
+        'A':1,
+        'B': 2,
+        'C': 3,
+        'D': 4,
+        'E': 5,
+        'F': 6,
+        'G': 7,
+        'H': 8,
+        'I': 9,
+        'J': 10,
+        'K': 11,
+        'L': 12,
+        'M': 13,
+        'N': 14,
+        'O': 15,
+        'P': 16,
+        'Q': 17,
+        'R': 18,
+        'S': 19,
+        'T': 20,
+        'U': 21,
+        'V': 22,
+        'W': 23,
+        'X': 24,
+        'Y': 25,
+        'Z': 26,
         }
-        for elem in columnTitle:
-            if elem in map:
-                pass
+        for i in range(0,len(columnTitle)):
+            num = map.get(columnTitle[len(columnTitle)-i-1])
+            result += num * (26 ** i)
+        return result
+
+
+
+
+solution = Solution()
+print(solution.titleToNumber('A'))
+print(solution.titleToNumber('AB'))
+print(solution.titleToNumber('ZY'))
