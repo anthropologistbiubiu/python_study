d615492d7cd2635954ec1220d841b026e79e47db
anthropologistbiubiu <1319847957@qq.com>
2024-08-28 00:25:21 +0800

    study

--- algorithm/exist/exist.py
+++ algorithm/exist/exist.py
@@ -4,18 +4,21 @@ from typing import List
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
-        cur = 0
-        self.dfs(board,word,0,0,0)
+        return self.dfs(board,word,0,0,0)
 
-
-    def dfs(self,board:List[List[str]],word:str,i:int,j :int, cur:int):
-        if board[i][j] != word[cur] or i <= 0 or j <= 0 or j == len(board[0]) or i == len(board) :
-            return
+    def dfs(self, board: List[List[str]], word: str, i: int, j: int, cur: int) -> bool:
+        if cur == len(word):
+            return True
+        if i < 0 or j < 0 or j == len(board[0]) or i == len(board):
+            return False
+        if board[i][j] != word[cur]:
+            pass
         else:
-            cur+=1
-        self.dfs(board,word,i-1,j,cur)
-        self.dfs(board,word,i+1,j,cur)
-        self.dfs(board,word,i,j-1,cur)
-        self.dfs(board,word,i,j+1,cur)
+            cur += 1
+        return self.dfs(board, word, i - 1, j, cur) or self.dfs(board, word, i + 1, j, cur) or self.dfs(board, word, i, j - 1, cur) or self.dfs(board, word, i, j + 1, cur)
 
 
+board = [["A", "B", "C", "E"], ["S", "F", "C", "S"], ["A", "D", "E", "E"]]
+word = "ABCCED"
+solution = Solution()
+print(solution.exist(board, word))
