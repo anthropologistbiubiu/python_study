6e9bcf0adfe45ecba4b8e9a145ce51ac3dd2ea0e
anthropologistbiubiu <1319847957@qq.com>
2024-08-27 22:46:32 +0800

    study

--- algorithm/exist/exist.py
+++ algorithm/exist/exist.py
@@ -5,3 +5,8 @@ from typing import List
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         pass
+
+
+    def dfs(self,board:List[List[str]],word:str,i:int,j :int, cur:int):
+        if board[i][j] != word[cur]:
+            return
