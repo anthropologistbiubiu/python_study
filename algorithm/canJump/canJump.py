4e21fbb3feaa51b664bf315e79ba30ae7050e4ac
anthropologistbiubiu <1319847957@qq.com>
2024-08-29 21:51:04 +0800

    study

--- algorithm/canJump/canJump.py
+++ algorithm/canJump/canJump.py
@@ -2,4 +2,10 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        pass
+        return True
+
+
+nums = [2, 3, 1, 1, 4]
+
+solution = Solution()
+print(solution.canJump(nums))
