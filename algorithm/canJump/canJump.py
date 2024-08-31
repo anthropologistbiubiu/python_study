fa3eacd4039ca1fbd544811a44303f5c2bed6b4b
anthropologistbiubiu <1319847957@qq.com>
2024-08-31 14:59:50 +0800

    study

--- algorithm/canJump/canJump.py
+++ algorithm/canJump/canJump.py
@@ -2,15 +2,16 @@ from typing import List
 
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
-        dp = [0 for _ in range (0,len(nums))]
-        dp[0] = nums[0]
-        for num in nums:
-            print(num)
-
-
+        max_reachable = 0
+        for index, num in enumerate(nums):
+            if index > max_reachable:
+                return False
+            max_reachable = num + index if num + index > max_reachable else max_reachable
+            if max_reachable >= len(nums) -1:
+                return True
+        return False
 
 
 nums = [2, 3, 1, 1, 4]
-
 solution = Solution()
-solution.canJump(nums)
+print(solution.canJump(nums))
