d8129c146364533e9a3032d550914d18d81d17c9
anthropologistbiubiu <1319847957@qq.com>
2024-09-05 23:51:37 +0800

    study

--- algorithm/canReach/canReach.py
+++ algorithm/canReach/canReach.py
@@ -1,7 +1,15 @@
-
 from typing import List
 
 
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
-        pass
+        queue = []
+        while len(queue) > 0:
+            pass
+
+
+arr = [4, 2, 3, 0, 3, 1, 2]
+start = 5
+
+solution = Solution()
+print(solution.canReach(arr,start))
