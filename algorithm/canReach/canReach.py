fe69159ab8a6892213c22649066e93257957e394
anthropologistbiubiu <1319847957@qq.com>
2024-09-07 14:25:39 +0800

    study

--- algorithm/canReach/canReach.py
+++ algorithm/canReach/canReach.py
@@ -4,12 +4,24 @@ from typing import List
 class Solution:
     def canReach(self, arr: List[int], start: int) -> bool:
         queue = []
-        while len(queue) > 0:
-            pass
+        exist = set()
+        queue.append(start)
+        while queue is not None:
+            elem = queue.pop()
+            print(elem)
+            if arr[elem] == 0:
+                return True
+            if elem in exist:
+                continue
+            if start - arr[start] > 0:
+                queue.append(start - arr[start])
+            if start + arr[start] < len(arr) - 1:
+                queue.append(start + arr[start])
+        return False
 
 
 arr = [4, 2, 3, 0, 3, 1, 2]
 start = 5
 
 solution = Solution()
-print(solution.canReach(arr,start))
+print(solution.canReach(arr, start))
