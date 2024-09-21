0e2dfafd5f1271801022466dd8824eeef8f0e9c3
anthropologistbiubiu <1319847957@qq.com>
2024-09-22 02:38:06 +0800

    study

--- algorithm/isIsomorphic/isIsomorphic.py
+++ algorithm/isIsomorphic/isIsomorphic.py
@@ -5,18 +5,25 @@
 
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return True
         s_to_t = {}
         t_to_s = {}
-        for index,item in enumerate(s):
-            s_to_t[item] = t[index]
-            t_to_s[t[index]] = item
-        for index,item in s_to_t.items():
-            if index != s_to_t.get(item) or item != t_to_s.get(index):
-                return False
+        for index in range (0,len(s)):
+            char_s = s[index]
+            char_t = t[index]
+            if char_s in s_to_t:
+                if char_t != s_to_t.get(char_s):
+                    return False
+            else:
+                s_to_t[char_s] = char_t
+            if char_t in t_to_s:
+                if char_s != t_to_s.get(char_t):
+                    return False
+            else:
+                t_to_s[char_t] = char_s
         return True
 
 solution = Solution()
-solution.isIsomorphic('eggp','affg')
-
-
-
+print(solution.isIsomorphic('egg', 'aff'))
+# print(solution.isIsomorphic('foo','bar'))
