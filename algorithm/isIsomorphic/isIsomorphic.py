1ba0fbd682cc58f980ca9917b851745e73561a11
anthropologistbiubiu <1319847957@qq.com>
2024-09-21 23:42:35 +0800

    study

--- algorithm/isIsomorphic/isIsomorphic.py
+++ algorithm/isIsomorphic/isIsomorphic.py
@@ -7,3 +7,7 @@ class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
         s_to_t = {}
         t_to_s = {}
+        for index,item in enumerate(s):
+            s_to_t[item] = t[index]
+            t_to_s[t[index]] = item
+
