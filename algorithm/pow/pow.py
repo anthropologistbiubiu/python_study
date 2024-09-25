622c7d40ff5f3a8b1d5b6786864a814b7ce45fc9
anthropologistbiubiu <1319847957@qq.com>
2024-09-25 22:53:01 +0800

    study

--- algorithm/pow/pow.py
+++ algorithm/pow/pow.py
@@ -2,4 +2,17 @@
 
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        pass
+        if n == 0:
+            return 1
+        elif n < 0:
+            return 1 / self.myPow(x, -n)
+        half = self.myPow(x, n // 2)
+        if n % 2 == 1:
+            return half * half * x
+        return half * half
+
+
+
+
+solution = Solution()
+print(solution.myPow(2,10))
