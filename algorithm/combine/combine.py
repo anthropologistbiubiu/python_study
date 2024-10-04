0914da1077522ae1d522cf4368677545d3156b8a
anthropologistbiubiu <1319847957@qq.com>
2024-10-04 15:35:18 +0800

    study

--- algorithm/combine/combine.py
+++ algorithm/combine/combine.py
@@ -1,8 +1,20 @@
 from typing import List
+
 class Solution:
 
     def combine(self, n: int, k: int) -> List[List[int]]:
         result = []
+        comb = []
 
-        def dfs(self,start ,n,lenght, int):
-            pass
+        def dfs(start: int, comb: List[int]):
+            if len(comb) == k:
+                result.append(comb.copy())
+            for i in range(start, n + 1):
+                comb.append(i)
+                dfs(start=i + 1, comb=comb)
+                comb.pop()
+        dfs(1, comb)
+        return result
+
+solution = Solution()
+print(solution.combine(n=5,k=3))
