84ce62dcebac5adc3c0a966214fd608054987670
anthropologistbiubiu <1319847957@qq.com>
2024-10-04 16:07:00 +0800

    study

--- algorithm/combinationSum/combinationSum.py
+++ algorithm/combinationSum/combinationSum.py
@@ -5,5 +5,7 @@ from typing import List
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
 
-        dfs(index: int,comb:List[int]):
-            pass
+        result = []
+        def dfs(i: int, total: int, comb: List[int]):
+            if total + candidates[i] == target:
+                result.append(comb.copy())
