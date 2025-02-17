db2faf969057a37fa9432b7b263a6a8d3a784658
anthropologistbiubiu <1319847957@qq.com>
2025-02-17 22:03:13 +0800

    improve

--- algorithm/maxRepeating/maxRepeating.py
+++ algorithm/maxRepeating/maxRepeating.py
@@ -3,4 +3,18 @@
 
 class Solution:
     def maxRepeating(self, sequence: str, word: str) -> int:
-        pass
+        n = len(sequence)
+        m = len(word)
+        max_repeat = 0
+        for i in range(n):
+            j = i
+            repeat = 0
+            while  i + m < n and sequence[i:i+n] == word:
+                repeat += 1
+                i += m
+                max_repeat = max(repeat,max_repeat)
+        return max_repeat
+
+
+
+
