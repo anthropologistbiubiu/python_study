87b25444da8f9d50d39dd80366ff1ea2fd6f60d7
anthropologistbiubiu <1319847957@qq.com>
2025-02-22 17:26:45 +0800

    study

--- algorithm/spiralOrder/spiralOrder.py
+++ algorithm/spiralOrder/spiralOrder.py
@@ -9,7 +9,19 @@ class Solution:
         right = n -1
         top = 0
         bottom = m -1
+        result = []
 
         while True:
-            pass
+            for j in range (left ,right + 1):
+                result.append(matrix[top][j])
+            top += 1 
+            for i in range (top,bottom + 1):
+                result .append(matrix[i][right]) 
+            right-=1
+            for j in range(right,left):
+                result.append(matrix[bottom][j])
+            bottom-=1
+            for i in range(bottom,top):
+                result.append(matrix[i][left]) 
+            left+=1
 
