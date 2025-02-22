5bc33909da1d18a4b9e47b13b6ce931aff7b611b
anthropologistbiubiu <1319847957@qq.com>
2025-02-22 17:19:33 +0800

    study

---
+++ algorithm/spiralOrder/spiralOrder.py
@@ -0,0 +1,15 @@
+
+class Solution:
+    def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
+
+        m = len(matrix)
+        n = len(matrix[0])
+
+        left = 0
+        right = n -1
+        top = 0
+        bottom = m -1
+
+        while True:
+            pass
+
