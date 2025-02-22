4f34358174c7e0d8fa6bf6ccdc81494226f22009
anthropologistbiubiu <1319847957@qq.com>
2025-02-22 17:38:37 +0800

    study

--- algorithm/spiralOrder/spiralOrder.py
+++ algorithm/spiralOrder/spiralOrder.py
@@ -12,15 +12,24 @@ class Solution:
         result = []
 
         while True:
+            if left > right:
+                break
             for j in range (left ,right + 1):
                 result.append(matrix[top][j])
             top += 1 
+
+            if top > bottom:
+                break
             for i in range (top,bottom + 1):
                 result .append(matrix[i][right]) 
             right-=1
+            if right < left:
+                break
             for j in range(right,left):
                 result.append(matrix[bottom][j])
             bottom-=1
+            if bottom < top:
+                break
             for i in range(bottom,top):
                 result.append(matrix[i][left]) 
             left+=1
