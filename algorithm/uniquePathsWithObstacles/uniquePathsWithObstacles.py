97af4a6eb1d6fb76935277b07220595f7cac0c32
anthropologistbiubiu <1319847957@qq.com>
2025-02-23 15:12:26 +0800

    study

--- algorithm/uniquePathsWithObstacles/uniquePathsWithObstacles.py
+++ algorithm/uniquePathsWithObstacles/uniquePathsWithObstacles.py
@@ -1,6 +1,24 @@
-
+from typing import List
 
 
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
-        pass
+
+        m = len(obstacleGrid)
+        n = len(obstacleGrid[0])
+        dp = [[0] * n for _ in range(m)]
+
+        for i in range (m):
+            for j in range (n):
+                if i == 0 or j == 0:
+                    dp[i][j] = 1
+                if obstacleGrid[i][j] ==0:
+                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        return dp[m-1][n-1]
+
+
+
+
+
+
+
