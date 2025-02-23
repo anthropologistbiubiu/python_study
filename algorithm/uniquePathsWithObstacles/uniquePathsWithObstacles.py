fa1da925c83890981026ee7df60eb36f142fff24
anthropologistbiubiu <1319847957@qq.com>
2025-02-23 22:06:41 +0800

    study

--- algorithm/uniquePathsWithObstacles/uniquePathsWithObstacles.py
+++ algorithm/uniquePathsWithObstacles/uniquePathsWithObstacles.py
@@ -1,6 +1,5 @@
 from typing import List
 
-
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
 
@@ -10,15 +9,20 @@ class Solution:
 
         for i in range (m):
             for j in range (n):
-                if i == 0 or j == 0:
-                    dp[i][j] = 1
-                if obstacleGrid[i][j] ==0:
-                    dp[i][j] = dp[i-1][j] + dp[i][j-1]
+                if i == 0 and j == 0:
+                    if obstacleGrid[i][j] == 0:
+                        dp[i][j] =1 
+                        continue
+                    else:
+                        return 0
+                if obstacleGrid[i][j] == 0:
+                    if i == 0 and dp[i][j-1] > 0 or j == 0 and dp[i-1][j] > 0 :
+                        dp[i][j] = 1
+                    else:
+                        dp[i][j] = dp[i-1][j] + dp[i][j-1]
         return dp[m-1][n-1]
 
-
-
-
-
-
+# [[1,0]]
+for i in range(3):
+    print(i)
 
