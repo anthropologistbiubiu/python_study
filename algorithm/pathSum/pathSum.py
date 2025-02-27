784741559855159e7ecdc43f17cb1cf97f2fd84c
anthropologistbiubiu <1319847957@qq.com>
2025-02-27 22:01:28 +0800

    study

--- algorithm/pathSum/pathSum.py
+++ algorithm/pathSum/pathSum.py
@@ -4,4 +4,11 @@
 
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        pass
+        path = []
+        self.dfs(root,targetSum=targetSum)
+        return path
+    def dfs(self, root: Optional[TreeNode], targetSum: int):
+        if root is None:
+            return 
+        if root.left not None:
+            self.pathSum(root.left,targetSum=targetSum-root.value)
