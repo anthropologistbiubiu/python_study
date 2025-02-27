c00f6e090b4f1242d774d44a175d9e92b8ef8e45
anthropologistbiubiu <1319847957@qq.com>
2025-02-28 01:03:29 +0800

    study

--- algorithm/pathSum/pathSum.py
+++ algorithm/pathSum/pathSum.py
@@ -1,14 +1,35 @@
 
+from typing import Optional
+from typing import List
 
 
+class TreeNode:
+
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        path = []
-        self.dfs(root,targetSum=targetSum)
-        return path
-    def dfs(self, root: Optional[TreeNode], targetSum: int):
         if root is None:
-            return 
-        if root.left not None:
-            self.pathSum(root.left,targetSum=targetSum-root.value)
+            return  []
+        path = []
+        numbers = []
+        self.dfs(root,targetSum,path,numbers)
+        return path
+
+    def dfs(self, root: Optional[TreeNode], targetSum: int,path: List[List[int]],numbers:List[int]):
+
+        if root.left is None and root.right is None:
+            if targetSum == root.val:
+                numbers.append(root.val)
+                path.append(numbers[:])
+                numbers.pop()
+            return
+        numbers.append(root.val)
+        if not root.left:
+            self.dfs(root.left,targetSum-root.val,path,numbers)
+        if not root.right :
+            self.dfs(root.right,targetSum-root.val,path,numbers)
+        numbers.pop()
