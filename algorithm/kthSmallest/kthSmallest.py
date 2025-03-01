d4c8b4b72981f8985ccce8ff901634b443ff1688
anthropologistbiubiu <1319847957@qq.com>
2025-03-02 00:05:57 +0800

    study

--- algorithm/kthSmallest/kthSmallest.py
+++ algorithm/kthSmallest/kthSmallest.py
@@ -1,9 +1,4 @@
-
 from typing import Optional
-
-
-
-
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -12,3 +7,20 @@ from typing import Optional
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        if not root:
+            return 0
+        result = [0]
+        self.dfs(root,1,k,result)
+        return result[0]
+
+    def dfs(self, root:Optional[TreeNode],i:int,k:int, result:List[int]) -> int:
+        if not root:
+            return
+        self.dfs(root.left,i,k,result)
+        if i == k:
+             result.append(root.val)
+        else:
+            i+=1
+        self.dfs(root.right,i,k,result)
+        
+        
