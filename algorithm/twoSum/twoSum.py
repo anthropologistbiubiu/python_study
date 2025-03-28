6908c34045710a7ed4aa6bf32488b2bdda72de3c
anthropologistbiubiu <1319847957@qq.com>
2025-03-28 23:04:48 +0800

    improve

--- algorithm/twoSum/twoSum.py
+++ algorithm/twoSum/twoSum.py
@@ -1,6 +1,22 @@
 
 
 
+from typing import List
+
 class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        pass
+        for i,value in enumerate(numbers):
+            left = i + 1
+            right = len(numbers) - 1
+            while left <= right:
+                mid = (left + right)  // 2
+                if numbers[mid] > target - value:
+                    right = mid -1
+                elif numbers[mid] < target -value:
+                    left = mid + 1
+                else:
+                    return [i+1,mid+1]
+        return []
+
+solution = Solution()
+print(solution.twoSum([1,2,3,4],3))
