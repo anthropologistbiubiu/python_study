f6065b78c1cc9524a859ce6fec3ffd0af7bb9500
anthropologistbiubiu <1319847957@qq.com>
2025-04-14 21:20:05 +0800

    study

--- algorithm/threeSumClosest/threeSumClosest.py
+++ algorithm/threeSumClosest/threeSumClosest.py
@@ -1,6 +1,34 @@
 
+from typing import List
 
 
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
-        pass
+
+        nums.sort()
+        closest_sum = float('inf')
+        for index,num in enumerate(nums):
+            left = index + 1 
+            right = len(nums) - 1 
+            while left < right:
+                current_sum = nums[left] + nums[right] + num
+                if abs(current_sum - target) < abs (closest_sum -target):
+                    closest_sum = current_sum
+                if current_sum < target:
+                    left += 1
+                elif current_sum > target:
+                    right -= 1
+                else:
+                    return current_sum
+        return closest_sum
+            
+                
+             
+
+
+
+solution = Solution()
+
+print(solution.threeSumClosest([1,2,3,4],5))
+            
+    
