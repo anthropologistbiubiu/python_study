7130b2b89c1411ac853b2d88940508561afc2ea5
anthropologistbiubiu <1319847957@qq.com>
2025-07-08 22:19:03 +0800

    移除 .gitignore 中忽略的所有文件

--- hello-ci-cd/tests/test_main.py
+++
@@ -1,9 +0,0 @@
-from fastapi.testclient import TestClient
-from app.main import app
-
-client = TestClient(app)
-
-def test_root():
-    r = client.get("/hello")
-    assert r.status_code == 200
-    assert r.json() == {"msg": "Hello, CI/CD"}
