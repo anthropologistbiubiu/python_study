8bcd438c7334178cb7c1aa78dffb2e7a2ed6b4d1
anthropologistbiubiu <1319847957@qq.com>
2025-07-11 00:08:35 +0800

    study

--- algorithm/combine_two_tables/combine_two_tables.py
+++ algorithm/combine_two_tables/combine_two_tables.py
@@ -18,13 +18,14 @@ addresses = pd.DataFrame({
 
 result = pd.merge(
     persons,
-    addresses[['PersonId', 'City', 'State']],  # 只取需要的列
+    #addresses[['PersonId', 'City', 'State']],  # 只取需要的列
+    addresses,  # 只取需要的列
     on='PersonId',
     how='left'  # 保留 persons 中所有记录，address 缺失时 City/State 为 NaN
 )
 
 
-result = result[['LastName', 'FirstName', 'City', 'State']]
+# result = result[['LastName', 'FirstName', 'City', 'State']]
 
 # --- 打印结果 ---
 print(result.to_string(index=False))
