dd53985ecc7442b56b5c5bad646e6df6b47f0df3
anthropologistbiubiu <1319847957@qq.com>
2025-07-16 20:33:09 +0800

    algorithm

--- algorithm/find_employees/find_employees.py
+++ algorithm/find_employees/find_employees.py
@@ -2,5 +2,14 @@
 
 import pandas as pd
 
+data = {
+    'id': [1, 2, 3, 4],
+    'name': ['Joe', 'Henry', 'Sam', 'Max'],
+    'salary': [70000, 80000, 60000, 90000],
+    'managerId': [3, 4, None, None]
+}
+employee = pd.DataFrame(data)
+print(employee)
+
 def find_employees(employee: pd.DataFrame) -> pd.DataFrame:
     pass
