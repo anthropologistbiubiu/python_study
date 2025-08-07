7fa4f1c8bf4df3e58253a59dfa3c0c084c326b0b
anthropologistbiubiu <1319847957@qq.com>
2025-08-07 22:33:44 +0800

    study

--- algorithm/duplicate_emails/duplicate_emails.py
+++ algorithm/duplicate_emails/duplicate_emails.py
@@ -1,5 +1,17 @@
-
 import pandas as pd
 
-def duplicate_emails(person: pd.DataFrame) -> pd.DataFrame:
-    pass
+# 输入数据
+data = {
+    'id': [1, 2, 3],
+    'email': ['a@b.com', 'c@d.com', 'a@b.com']
+}
+df = pd.DataFrame(data)
+
+# 统计邮箱出现次数
+email_counts = df['email'].value_counts()
+
+# 取出重复的邮箱地址
+duplicate_emails = email_counts[email_counts > 1].index
+# 构建结果 DataFrame 并按要求重命名列
+result = pd.DataFrame(duplicate_emails, columns=['Email'])
+
