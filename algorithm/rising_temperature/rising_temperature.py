6fd5c17c2f537bbaa667b096a952cf93ea4318e1
anthropologistbiubiu <1319847957@qq.com>
2025-08-14 21:46:11 +0800

    algorithm

--- algorithm/rising_temperature/rising_temperature.py
+++ algorithm/rising_temperature/rising_temperature.py
@@ -1,7 +1,13 @@
-
-
 import pandas as pd
 
 
 def rising_temperature(weather: pd.DataFrame) -> pd.DataFrame:
-    pass
+    merged = weather.merge(
+        weather[["id", "recordDate", "temperature"]],
+        left_on="recordDate",
+        right_on=weather["recordDate"] + pd.Timedelta(days=1),
+        how="inner",
+        suffixes=("", "_y"),
+    )
+    filtered = merged[merged["temperature"] > merged["temperature_y"]]
+    return filtered[["id"]].rename(columns={"id": "Id"})
