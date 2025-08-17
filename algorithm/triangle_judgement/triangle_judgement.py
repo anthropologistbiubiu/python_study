1e9f54dd41609ea28a9acb6fbc0ae9325acea262
anthropologistbiubiu <1319847957@qq.com>
2025-08-17 18:44:20 +0800

    triangle_judgement

--- algorithm/triangle_judgement/triangle_judgement.py
+++ algorithm/triangle_judgement/triangle_judgement.py
@@ -4,4 +4,20 @@ import pandas as pd
 
 
 def triangle_judgement(triangle: pd.DataFrame) -> pd.DataFrame:
-    pass
+
+    is_triangle = (triangle['a'] + triangle['b'] > triangle['c']) & (
+        triangle['a'] + triangle['c'] > triangle['b']) & (
+        triangle['b'] + triangle['c'] > triangle['a']
+    )
+    triangle['triangle'] = is_triangle.map(lambda x: 'Yes' if x else 'No')
+
+    return triangle
+
+
+triangle = pd.DataFrame({
+    "a": [3, 1, 2],
+    "b": [4, 2, 3],
+    "c": [5, 3, 5]
+})
+
+print(triangle_judgement(triangle))
