17e600bdb97f86e4687e074fc89571f89b975bfc
anthropologistbiubiu <1319847957@qq.com>
2025-09-07 21:52:14 +0800

    study

--- algorithm/reformat_table/reformat_table.py
+++ algorithm/reformat_table/reformat_table.py
@@ -2,4 +2,16 @@ import pandas as pd
 
 
 def reformat_table(department: pd.DataFrame) -> pd.DataFrame:
-    pass
+    print(department)
+
+
+data = {
+    "id": [1, 2, 3, 1, 1],
+    "revenue": [8000, 9000, 10000, 7000, 6000],
+    "month": ["Jan", "Jan", "Feb", "Feb", "Mar"]
+}
+
+df = pd.DataFrame(data)
+
+
+reformat_table(df)
