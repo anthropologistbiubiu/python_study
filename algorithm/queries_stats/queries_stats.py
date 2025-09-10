18dbc9994cc6f7936914ccef19c018a3e9a4e393
anthropologistbiubiu <1319847957@qq.com>
2025-09-10 22:25:46 +0800

    improve

--- algorithm/queries_stats/queries_stats.py
+++ algorithm/queries_stats/queries_stats.py
@@ -3,3 +3,17 @@ import pandas as pd
 
 def queries_stats(queries: pd.DataFrame) -> pd.DataFrame:
     pass
+
+
+data = {
+    "query_name": ["Dog", "Dog", "Dog", "Cat", "Cat", "Cat"],
+    "result": ["Golden Retriever", "German Shepherd", "Mule",
+               "Shirazi", "Siamese", "Sphynx"],
+    "position": [1, 2, 200, 5, 3, 7],
+    "rating": [5, 5, 1, 2, 3, 4]
+}
+
+df = pd.DataFrame(data)
+
+
+print(queries_stats(df))
