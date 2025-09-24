6e077a4800c8056690df66e01c81952f127107f5
anthropologistbiubiu <1319847957@qq.com>
2025-09-24 20:42:44 +0800

    algorithm

---
+++ algorithm/find_patients/find_patients.py
@@ -0,0 +1,6 @@
+
+import pandas as pd
+
+
+def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
+    pass
--- algorithm/movie_rating/movie_rating.py
+++ algorithm/movie_rating/movie_rating.py
@@ -5,5 +5,24 @@ def movie_rating(movies: pd.DataFrame, users: pd.DataFrame, movie_rating: pd.Dat
     pass
 
 
-a = 10.000
-print(a)
+# 示例数据
+movies = pd.DataFrame({
+    "movie_id": [1, 2, 3],
+    "title": ["Avengers", "Frozen 2", "Joker"]
+})
+
+users = pd.DataFrame({
+    "user_id": [1, 2, 3, 4],
+    "name": ["Daniel", "Monica", "Maria", "James"]
+})
+
+movierating = pd.DataFrame({
+    "movie_id": [1, 1, 1, 1, 2, 2, 2, 3, 3],
+    "user_id":  [1, 2, 3, 4, 1, 2, 3, 1, 2],
+    "rating":   [3, 4, 2, 1, 5, 2, 2, 3, 4],
+    "created_at": pd.to_datetime([
+        "2020-01-12", "2020-02-11", "2020-02-12", "2020-01-01",
+        "2020-02-17", "2020-02-01", "2020-03-01",
+        "2020-02-22", "2020-02-25"
+    ])
+})
