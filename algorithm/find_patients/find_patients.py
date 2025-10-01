e8161f7019003909cde8dce5a6f59a45a9bf39b2
anthropologistbiubiu <1319847957@qq.com>
2025-10-01 23:59:55 +0800

    deleted:    actors_and_directors/actors_and_directors.py 	modified:   find_patients/find_patients.py 	modified:   replace_employee_id/replace_employee_id.py

--- algorithm/find_patients/find_patients.py
+++ algorithm/find_patients/find_patients.py
@@ -3,4 +3,20 @@ import pandas as pd
 
 
 def find_patients(patients: pd.DataFrame) -> pd.DataFrame:
-    pass
+
+    result = patients[patients["conditions"].str.contains(
+        pat=r"(^|\s)DIAB1.*")]
+    return result
+
+
+data = {
+    "patient_id": [1, 2, 3, 4, 5],
+    "patient_name": ["Daniel", "Alice", "Bob", "George", "Alain"],
+    "conditions": ["YFEV COUGH", "", "DIAB100 MYOP", "ACNE DIAB100", "DIAB201"]
+}
+patients = pd.DataFrame(data)
+
+
+if __name__ == "__main__":
+    result = find_patients(patients)
+    print(result)
--- algorithm/replace_employee_id/replace_employee_id.py
+++ algorithm/replace_employee_id/replace_employee_id.py
@@ -3,4 +3,25 @@ import pandas as pd
 
 
 def replace_employee_id(employees: pd.DataFrame, employee_uni: pd.DataFrame) -> pd.DataFrame:
-    pass
+    merged = employees.merge(
+        employee_uni,
+        on="id",
+        how="left"
+    )
+    result = merged[["unique_id", "name"]]
+    return result
+
+
+# 模拟数据
+employees = pd.DataFrame({
+    "id": [1, 7, 11, 90, 3],
+    "name": ["Alice", "Bob", "Meir", "Winston", "Jonathan"]
+})
+
+employee_uni = pd.DataFrame({
+    "id": [3, 11, 90],
+    "unique_id": [1, 2, 3]
+})
+
+if __name__ == "__main__":
+    print(replace_employee_id(employees, employee_uni))
