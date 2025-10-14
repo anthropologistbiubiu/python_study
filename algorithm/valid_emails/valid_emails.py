abd83e6aefbc96741545419e7f500460ba403d6b
anthropologistbiubiu <1319847957@qq.com>
2025-10-14 23:45:30 +0800

    修改：     valid_emails/valid_emails.py

--- algorithm/valid_emails/valid_emails.py
+++ algorithm/valid_emails/valid_emails.py
@@ -1,1 +1,9 @@
 
+
+import pandas as pd
+
+
+def valid_emails(users: pd.DataFrame) -> pd.DataFrame:
+    r = r"^[A-Za-z]{1,}[a-zA-Z0-9._-]*@leetcode\.com$"
+    result = users[users["mail"].str.match(r)]
+    return result
