caeb1a46c8d83602c5f6150c9ccb2c79de997854
anthropologistbiubiu <1319847957@qq.com>
2025-10-16 21:13:13 +0800

    修改：     categorize_products/categorize_products.py

---
+++ algorithm/categorize_products/categorize_products.py
@@ -0,0 +1,11 @@
+
+
+import pandas as pd
+
+# 模拟输入数据
+data = {
+    'sell_date': ['2020-05-30', '2020-06-01', '2020-06-02', '2020-05-30', '2020-06-01', '2020-06-02', '2020-05-30'],
+    'product':   ['Headphone', 'Pencil', 'Mask', 'Basketball', 'Bible', 'Mask', 'T-shirt']
+}
+
+df = pd.DataFrame(data)
