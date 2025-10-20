dcff8f6ac385a0787ab7fa7ef1e6b9ffa33c38c3
anthropologistbiubiu <1319847957@qq.com>
2025-10-20 23:47:42 +0800

    修改：     get_average_time/get_average_time.py

--- algorithm/get_average_time/get_average_time.py
+++ algorithm/get_average_time/get_average_time.py
@@ -5,3 +5,24 @@ import pandas as pd
 
 def get_average_time(activity: pd.DataFrame) -> pd.DataFrame:
     pass
+
+
+data = [
+    [0, 0, 'start', 0.712],
+    [0, 0, 'end', 1.520],
+    [0, 1, 'start', 3.140],
+    [0, 1, 'end', 4.120],
+    [1, 0, 'start', 0.550],
+    [1, 0, 'end', 1.550],
+    [1, 1, 'start', 0.430],
+    [1, 1, 'end', 1.420],
+    [2, 0, 'start', 4.100],
+    [2, 0, 'end', 4.512],
+    [2, 1, 'start', 2.500],
+    [2, 1, 'end', 5.000],
+]
+
+df = pd.DataFrame(data, columns=['machine_id',
+                  'process_id', 'activity_type', 'timestamp'])
+
+print(get_average_time(df))
