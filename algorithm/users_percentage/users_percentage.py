fa26d7c374665da05021802bca9b6d27721c3cff
anthropologistbiubiu <1319847957@qq.com>
2025-10-24 22:41:59 +0800

    修改：     users_percentage/users_percentage.py

--- algorithm/users_percentage/users_percentage.py
+++ algorithm/users_percentage/users_percentage.py
@@ -3,3 +3,14 @@ import pandas as pd
 
 def users_percentage(users: pd.DataFrame, register: pd.DataFrame) -> pd.DataFrame:
     pass
+
+
+users = pd.DataFrame({
+    "user_id": [1, 2, 3, 4],
+    "join_date": ["2020-02-14", "2020-02-14", "2020-03-01", "2020-04-01"]
+})
+
+register = pd.DataFrame({
+    "user_id": [1, 2, 2, 3],
+    "activity_date": ["2020-03-01", "2020-03-02", "2020-03-03", "2020-03-02"]
+})
