3ba1a6aa67b013f6332e267a862b0be9b2192ad5
anthropologistbiubiu <1319847957@qq.com>
2025-10-26 15:54:26 +0800

    修改：     users_percentage/users_percentage.py

--- algorithm/users_percentage/users_percentage.py
+++ algorithm/users_percentage/users_percentage.py
@@ -2,15 +2,28 @@ import pandas as pd
 
 
 def users_percentage(users: pd.DataFrame, register: pd.DataFrame) -> pd.DataFrame:
-    pass
+    register_user = register.groupby(
+        ["contest_id"])["user_id"].count().reset_index()
+
+    register_user["total"] = users["user_id"].nunique()
+    register_user["percentage"] = register_user["user_id"] / \
+        register_user["total"] * 100
+    register_user["percentage"] = register_user["percentage"].round(2)
+    register_user = register_user.sort_values(
+        by=["percentage", "contest_id"], ascending=[False, True])
+    result = register_user[["contest_id", "percentage"]]
+    return result
 
 
 users = pd.DataFrame({
-    "user_id": [1, 2, 3, 4],
-    "join_date": ["2020-02-14", "2020-02-14", "2020-03-01", "2020-04-01"]
+    "user_id": [6, 2, 7],
+    "user_name": ["Alice", "Bob", "Alex"]
 })
 
 register = pd.DataFrame({
-    "user_id": [1, 2, 2, 3],
-    "activity_date": ["2020-03-01", "2020-03-02", "2020-03-03", "2020-03-02"]
+    "contest_id": [215, 209, 208, 210, 208, 209, 209, 215, 208, 210, 207, 210],
+    "user_id":    [6,  2,  2,  6,  6,  7,  6,  7,  7,  2,  2,  7]
 })
+
+
+print(users_percentage(users=users, register=register))
