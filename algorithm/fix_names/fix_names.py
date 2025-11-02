2af928f4a85bd36c3e8558d9ea09a58116431312
anthropologistbiubiu <1319847957@qq.com>
2025-11-02 14:30:36 +0800

    修改：     fix_names/fix_names.py

--- algorithm/fix_names/fix_names.py
+++ algorithm/fix_names/fix_names.py
@@ -3,5 +3,19 @@
 import pandas as pd
 
 
+data = {
+    'user_id': [1, 2],
+    'name': ['aLice', 'bOB']
+}
+
+df = pd.DataFrame(data)
+
+
 def fix_names(users: pd.DataFrame) -> pd.DataFrame:
-    pass
+    users["name"] = users["name"].str.capitalize()
+    users.sort_values(by=["name"], inplace=True)
+    return users
+
+
+if __name__ == "__main__":
+    print(fix_names(df))
