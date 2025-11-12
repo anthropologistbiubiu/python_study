297018613f52b22acd4bbd5d499104fedf30de68
anthropologistbiubiu <62462764+anthropologistbiubiu@users.noreply.github.com>
2025-11-12 17:03:12 +0800

    Implement count_followers function with sample data
    Added a sample DataFrame and implemented the count_followers function to aggregate follower counts.

--- algorithm/count_followers/count_followers.py
+++ algorithm/count_followers/count_followers.py
@@ -1,7 +1,20 @@
-
-
 import pandas as pd
 
 
+df = pd.DataFrame({"user_id": [0, 1, 2, 2], "follower_id": [1, 0, 0, 1]})
+
+
+print(df)
+
+
 def count_followers(followers: pd.DataFrame) -> pd.DataFrame:
-    pass
+    result = (
+        followers.groupby(["user_id"])
+        .agg(followers=("follower_id", "count"))
+        .reset_index()
+    )
+    return result
+
+
+if __name__ == "__main__":
+    count_followers(df)
