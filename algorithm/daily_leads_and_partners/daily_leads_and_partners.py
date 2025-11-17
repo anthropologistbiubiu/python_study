7d08e4a6b3650d8ef38812b5834567e384016433
anthropologistbiubiu <62462764+anthropologistbiubiu@users.noreply.github.com>
2025-11-17 10:20:23 +0800

    Implement daily_leads_and_partners function

--- algorithm/daily_leads_and_partners/daily_leads_and_partners.py
+++ algorithm/daily_leads_and_partners/daily_leads_and_partners.py
@@ -1,7 +1,50 @@
-
-
 import pandas as pd
 
 
 def daily_leads_and_partners(daily_sales: pd.DataFrame) -> pd.DataFrame:
-    pass
+    result = (
+        daily_sales.groupby(["date_id", "make_name"])
+        .agg(
+            unique_leads=("lead_id", "nunique"),
+            unique_partners=("partner_id", "nunique"),
+        )
+        .reset_index()
+        .sort_values(by=["make_name", "date_id"], ascending=[True, False])
+    )
+    return result
+
+
+import pandas as pd
+
+data = {
+    "date_id": [
+        "2020-12-8",
+        "2020-12-8",
+        "2020-12-8",
+        "2020-12-7",
+        "2020-12-7",
+        "2020-12-8",
+        "2020-12-8",
+        "2020-12-7",
+        "2020-12-7",
+        "2020-12-7",
+    ],
+    "make_name": [
+        "toyota",
+        "toyota",
+        "toyota",
+        "toyota",
+        "toyota",
+        "honda",
+        "honda",
+        "honda",
+        "honda",
+        "honda",
+    ],
+    "lead_id": [0, 1, 1, 0, 0, 1, 2, 0, 1, 2],
+    "partner_id": [1, 0, 2, 2, 1, 2, 1, 1, 2, 1],
+}
+
+df = pd.DataFrame(data)
+
+print(daily_leads_and_partners(df))
