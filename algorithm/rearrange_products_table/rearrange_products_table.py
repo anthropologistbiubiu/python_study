1a926aa24c5de5114b20d86266a969fe69e548d1
anthropologistbiubiu <62462764+anthropologistbiubiu@users.noreply.github.com>
2025-11-19 16:32:45 +0800

    Add rearrange_products_table function
    Implement function to rearrange product prices by store.

--- algorithm/rearrange_products_table/rearrange_products_table.py
+++ algorithm/rearrange_products_table/rearrange_products_table.py
@@ -1,4 +1,32 @@
 import pandas as pd
 
+
+data = {
+    "product_id": [0, 1],
+    "store1": [95, 70],
+    "store2": [100, None],
+    "store3": [105, 80],
+}
+
+
+df = pd.DataFrame(data)
+
+
 def rearrange_products_table(products: pd.DataFrame) -> pd.DataFrame:
-    pass
+    result = pd.melt(
+        products,
+        id_vars="product_id",
+        var_name="store",
+        value_vars=["store1", "store2", "store3"],
+        value_name="price",
+    )
+    result = (
+        result[result["price"].notnull()]
+        .sort_values(by=["product_id", "store"])
+        .reset_index(drop=True)
+    )
+    return result
+
+
+if __name__ == "__main__":
+    print(rearrange_products_table(df))
