c3a423149d33bd459e6a207d8ffea1ca0fc67aae
anthropologistbiubiu <62462764+anthropologistbiubiu@users.noreply.github.com>
2025-11-19 17:22:51 +0800

    Add total_time function to calculate employee hours
    Implement total_time function to calculate total working hours for employees by aggregating in and out times.

--- algorithm/total_time/total_time.py
+++ algorithm/total_time/total_time.py
@@ -1,7 +1,19 @@
-
-
 import pandas as pd
 
-
 def total_time(employees: pd.DataFrame) -> pd.DataFrame:
-    pass
+    try:
+        employees["total_time"] = employees["out_time"] - employees["in_time"]
+
+        result = (
+            employees.groupby(["emp_id", "event_day"])
+            .agg(total_time=("total_time", "sum"))
+            .reset_index()
+            .rename(columns={"event_day": "day"})
+        )
+        result = result[["day", "emp_id", "total_time"]]
+        result = result.sort_values(by=["day"])
+        return result
+
+    except KeyError as e:
+        print(f"KeyError: {e}")
+    return None
