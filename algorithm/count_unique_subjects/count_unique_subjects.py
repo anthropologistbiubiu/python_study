962a8976fbacd573fd7ce109977715c7e0910894
anthropologistbiubiu <62462764+anthropologistbiubiu@users.noreply.github.com>
2025-11-24 11:44:44 +0800

    Implement count_unique_subjects function

--- algorithm/count_unique_subjects/count_unique_subjects.py
+++ algorithm/count_unique_subjects/count_unique_subjects.py
@@ -3,5 +3,9 @@
 import pandas as pd
 
 
+
 def count_unique_subjects(teacher: pd.DataFrame) -> pd.DataFrame:
-    pass
+    grouped = (
+        teacher.groupby(["teacher_id"])["subject_id"].nunique().reset_index(name="cnt")
+    )
+    return grouped
