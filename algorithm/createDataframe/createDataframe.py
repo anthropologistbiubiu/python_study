cf71a93e2f54f1a2d7a811be2b7d05220b518148
anthropologistbiubiu <62462764+anthropologistbiubiu@users.noreply.github.com>
2025-11-26 16:48:35 +0800

    Implement createDataframe function to return DataFrame

--- algorithm/createDataframe/createDataframe.py
+++ algorithm/createDataframe/createDataframe.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+from typing import List
 
 def createDataframe(student_data: List[List[int]]) -> pd.DataFrame:
-    pass
+    return pd.DataFrame(student_data, columns=["student_id", "age"])
