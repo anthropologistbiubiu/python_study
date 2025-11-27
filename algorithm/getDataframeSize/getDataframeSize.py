abceec12e80f9b3218106b9e3f102643ded4303b
anthropologistbiubiu <62462764+anthropologistbiubiu@users.noreply.github.com>
2025-11-27 15:01:42 +0800

    Add function to get size of DataFrame

--- algorithm/getDataframeSize/getDataframeSize.py
+++ algorithm/getDataframeSize/getDataframeSize.py
@@ -1,1 +1,55 @@
+import pandas as pd
+from typing import List
 
+
+data = {
+    "player_id": [846, 749, 155, 583, 388, 883, 355, 247, 761, 642],
+    "name": [
+        "Mason",
+        "Riley",
+        "Bob",
+        "Isabella",
+        "Zachary",
+        "Ava",
+        "Violet",
+        "Thomas",
+        "Jack",
+        "Charlie",
+    ],
+    "age": [21, 30, 28, 32, 24, 23, 18, 27, 33, 36],
+    "position": [
+        "Forward",
+        "Winger",
+        "Striker",
+        "Goalkeeper",
+        "Midfielder",
+        "Defender",
+        "Striker",
+        "Striker",
+        "Midfielder",
+        "Center-back",
+    ],
+    "team": [
+        "RealMadrid",
+        "Barcelona",
+        "ManchesterUnited",
+        "Liverpool",
+        "BayernMunich",
+        "Chelsea",
+        "Juventus",
+        "ParisSaint-Germain",
+        "ManchesterCity",
+        "Arsenal",
+    ],
+}
+
+
+df = pd.DataFrame(data)
+
+
+def getDataframeSize(players: pd.DataFrame) -> List[int]:
+    return list(players.shape)
+
+
+if __name__ == "__main__":
+    print(getDataframeSize(df))  # Output: [10, 5]
