1390c8a5480bed125e520f3328842970ebaffe28
anthropologistbiubiu <62462764+anthropologistbiubiu@users.noreply.github.com>
2025-11-28 11:01:11 +0800

    Add function to create bonus column in DataFrame

--- algorithm/createBonusColumn/createBonusColumn.py
+++ algorithm/createBonusColumn/createBonusColumn.py
@@ -1,1 +1,18 @@
+import pandas as pd
 
+
+data = {
+    "name": ["Piper", "Grace", "Georgia", "Willow", "Finn", "Thomas"],
+    "salary": [4548, 28150, 1103, 6593, 74576, 24433],
+}
+
+
+def createBonusColumn(employees: pd.DataFrame) -> pd.DataFrame:
+    employees["bonus"] = employees["salary"] * 2
+    return employees
+
+
+employees = pd.DataFrame(data)
+
+if __name__ == "__main__":
+    print(createBonusColumn(pd.DataFrame(data)))
