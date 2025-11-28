7f670766f089f0554ee76e63e9a824acd6a0322d
anthropologistbiubiu <62462764+anthropologistbiubiu@users.noreply.github.com>
2025-11-28 10:49:54 +0800

    Add selectData function to filter student data

--- algorithm/selectData/selectData.py
+++ algorithm/selectData/selectData.py
@@ -1,1 +1,19 @@
+import pandas as pd
+
+
+data = {
+    "student_id": [101, 53, 128, 3],
+    "name": ["Ulysses", "William", "Henry", "Henry"],
+    "age": [13, 10, 6, 11],
+}
+
+
+def selectData(students: pd.DataFrame) -> pd.DataFrame:
+    return students.loc[students["student_id"] == 101, ["name", "age"]]
+
+
+students = pd.DataFrame(data)
+
+if __name__ == "__main__":
+    print(selectData(students))
 
