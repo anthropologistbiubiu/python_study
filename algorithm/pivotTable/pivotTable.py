d0e67b453f99a009b52a5e2f8ee5ec8b1835f1bc
anthropologistbiubiu <62462764+anthropologistbiubiu@users.noreply.github.com>
2025-12-02 15:07:55 +0800

    Update pivotTable.py

--- algorithm/pivotTable/pivotTable.py
+++ algorithm/pivotTable/pivotTable.py
@@ -1,1 +1,42 @@
+import pandas as pd
+
+
+def pivotTable(weather: pd.DataFrame) -> pd.DataFrame:
+    result = weather.pivot(index="month", columns="city", values="temperature")
+    return result
+
+
+data = {
+    "city": [
+        "Jacksonville",
+        "Jacksonville",
+        "Jacksonville",
+        "Jacksonville",
+        "Jacksonville",
+        "ElPaso",
+        "ElPaso",
+        "ElPaso",
+        "ElPaso",
+        "ElPaso",
+    ],
+    "month": [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+    ],
+    "temperature": [13, 23, 38, 5, 34, 20, 6, 26, 2, 43],
+}
+
+weather = pd.DataFrame(data)
+
+
+if __name__ == "__main__":
+    print(pivotTable(weather))
 
